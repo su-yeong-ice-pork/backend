747c2e57d871a6c6655bd79c8d212112607339fa
MinseokGo <rhalstjr1999@naver.com>
2024-09-20 18:10:50 +0900

    init: grass project

---
+++ src/main/java/develop/grassserver/GrassServerApplication.java
@@ -0,0 +1,13 @@
+package develop.grassserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrassServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/develop/grassserver/GrassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package develop.grassserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
