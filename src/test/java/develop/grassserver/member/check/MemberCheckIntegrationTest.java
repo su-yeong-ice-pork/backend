d9c32642f22726798aeb9101fed8dd53a5df37e4
MinseokGo <rhalstjr1999@naver.com>
2024-09-22 10:41:38 +0900

    test: 테스트 삭제

--- src/test/java/develop/grassserver/member/check/MemberCheckIntegrationTest.java
+++ src/test/java/develop/grassserver/member/check/MemberCheckIntegrationTest.java
@@ -21,34 +21,6 @@ public class MemberCheckIntegrationTest {
     MockMvc mockMvc;
 
     @Test
-    @DisplayName("성공 - 멤버 ID 중복 및 형식 체크")
-    void test1() throws Exception {
-        String memberId = "aaaa";
-        ResultActions result = mockMvc.perform(
-                get("/api/v1/members/check/id")
-                        .param("memberId", memberId)
-        );
-
-        result.andExpect(
-                        jsonPath("$.success").value("true"))
-                .andDo(print());
-    }
-
-    @Test
-    @DisplayName("실패 - 멤버 ID 길이 미충족")
-    void test2() throws Exception {
-        String memberId = "aaa";
-        ResultActions result = mockMvc.perform(
-                get("/api/v1/members/check/id")
-                        .param("memberId", memberId)
-        );
-
-        result.andExpect(
-                        jsonPath("$.success").value("false"))
-                .andDo(print());
-    }
-
-    @Test
     @DisplayName("성공 - 멤버 이름 중복 및 길이 충족")
     void test3() throws Exception {
         String name = "aaa";
