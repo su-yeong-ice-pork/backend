ba244f04f3f62841aefaf8285c9025cffb9790b9
MinseokGo <rhalstjr1999@naver.com>
2024-09-22 10:35:06 +0900

    refactor: 멤버 엔티티 수정

--- src/main/java/develop/grassserver/member/Member.java
+++ src/main/java/develop/grassserver/member/Member.java
@@ -19,14 +19,11 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("status = true")
 public class Member extends BaseEntity {
 
-    @Column(length = 16, nullable = false, unique = true)
+    @Column(length = 8, nullable = false, unique = true)
     private String name;
 
     @Column(length = 100, nullable = false, unique = true)
     private String email;
-
-    @Column(length = 10, nullable = false, unique = true)
-    private String loginId;
 
     @Column(length = 64, nullable = false)
     private String password;
