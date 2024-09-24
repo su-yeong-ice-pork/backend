9a54a65466730b7fe5db2dcdf166af30f0b429f1
MinseokGo <rhalstjr1999@naver.com>
2024-09-25 01:41:20 +0900

    feat: 인증코드 유효성 검사

--- src/main/java/develop/grassserver/member/auth/RedisService.java
+++ src/main/java/develop/grassserver/member/auth/RedisService.java
@@ -1,6 +1,10 @@
 package develop.grassserver.member.auth;
 
+import develop.grassserver.member.auth.exception.ExpirationAuthCodeException;
+import develop.grassserver.member.auth.exception.IncorrectAuthCodeException;
+import develop.grassserver.member.check.dto.CheckAuthCodeRequest;
 import java.time.Duration;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -18,5 +22,22 @@ public class RedisService {
         ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
         Duration duration = Duration.ofSeconds(AUTH_CODE_EXPIRATION_TIME);
         valueOperations.set(email, code, duration);
+    }
+
+    public void checkAuthCode(CheckAuthCodeRequest request) {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        String email = request.email();
+        String code = (String) valueOperations.get(email);
+        if (Objects.isNull(code)) {
+            throw new ExpirationAuthCodeException("인증코드가 만료되었습니다. 인증코드를 재발급해주세요.");
+        }
+        if (!AuthValidator.isCorrectAuthCode(code, request)) {
+            throw new IncorrectAuthCodeException("인증코드가 일치하지 않습니다.");
+        }
+        deleteAuthCode(email);
+    }
+
+    private void deleteAuthCode(String email) {
+        redisTemplate.delete(email);
     }
 }
--- src/main/java/develop/grassserver/member/check/MemberCheckController.java
+++ src/main/java/develop/grassserver/member/check/MemberCheckController.java
@@ -1,10 +1,14 @@
 package develop.grassserver.member.check;
 
 import develop.grassserver.mail.MailService;
+import develop.grassserver.member.auth.RedisService;
+import develop.grassserver.member.check.dto.CheckAuthCodeRequest;
 import develop.grassserver.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberCheckController {
 
     private final MailService mailService;
+    private final RedisService redisService;
     private final MemberCheckService memberCheckService;
 
     @GetMapping("/name")
@@ -28,6 +33,13 @@ public class MemberCheckController {
     public ResponseEntity<ApiUtils.ApiResult<?>> checkEmail(@RequestParam String email) {
         memberCheckService.checkMemberEmail(email);
         mailService.sendMail(email);
+        return ResponseEntity.ok()
+                .body(ApiUtils.success(null));
+    }
+
+    @PostMapping("/code")
+    public ResponseEntity<ApiUtils.ApiResult<?>> checkCode(@RequestBody CheckAuthCodeRequest request) {
+        redisService.checkAuthCode(request);
         return ResponseEntity.ok()
                 .body(ApiUtils.success(null));
     }
