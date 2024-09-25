1507b10b0fae71e92c22baf2a6a48bf014541002
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-25 16:59:53 +0900

    feat: jwtException  리턴 response 통일

--- src/main/java/develop/grassserver/member/exception/JwtExceptionHandler.java
+++ src/main/java/develop/grassserver/member/exception/JwtExceptionHandler.java
@@ -1,5 +1,6 @@
 package develop.grassserver.member.exception;
 
+import develop.grassserver.utils.ApiUtils;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
@@ -13,35 +14,37 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class JwtExceptionHandler {
 
-    private ResponseEntity<String> createJwtErrorResponse(String errorMessage) {
+    private ResponseEntity<ApiUtils.ApiResult<String>> createJwtErrorResponse(String errorMessage) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("WWW-Authenticate",
-                "Bearer realm=\"access\"");
-        return new ResponseEntity<>(errorMessage, headers, HttpStatus.UNAUTHORIZED);
+        headers.add("WWW-Authenticate", "Bearer realm=\"access\"");
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .headers(headers)
+                .body(ApiUtils.error(HttpStatus.UNAUTHORIZED, errorMessage));
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
-    public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException ex) {
+    public ResponseEntity<ApiUtils.ApiResult<String>>  handleExpiredJwtException(ExpiredJwtException ex) {
         return createJwtErrorResponse("토큰이 만료되었습니다.");
     }
 
     @ExceptionHandler(UnsupportedJwtException.class)
-    public ResponseEntity<String> handleUnsupportedJwtException(UnsupportedJwtException ex) {
+    public ResponseEntity<ApiUtils.ApiResult<String>>  handleUnsupportedJwtException(UnsupportedJwtException ex) {
         return createJwtErrorResponse("지원하지 않는 토큰 형식입니다.");
     }
 
     @ExceptionHandler(MalformedJwtException.class)
-    public ResponseEntity<String> handleMalformedJwtException(MalformedJwtException ex) {
+    public ResponseEntity<ApiUtils.ApiResult<String>>  handleMalformedJwtException(MalformedJwtException ex) {
         return createJwtErrorResponse("토큰의 형식이 올바르지 않습니다.");
     }
 
     @ExceptionHandler(SecurityException.class)
-    public ResponseEntity<String> handleSecurityException(SecurityException ex) {
+    public ResponseEntity<ApiUtils.ApiResult<String>>  handleSecurityException(SecurityException ex) {
         return createJwtErrorResponse("토큰의 서명이 유효하지 않습니다.");
     }
 
     @ExceptionHandler(JwtException.class)
-    public ResponseEntity<String> handleException(Exception ex) {
+    public ResponseEntity<ApiUtils.ApiResult<String>>  handleException(Exception ex) {
         return createJwtErrorResponse("유효하지 않은 토큰입니다.");
     }
 }
