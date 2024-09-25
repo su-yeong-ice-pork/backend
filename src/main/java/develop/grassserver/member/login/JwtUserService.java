8f1e4ba91d98027a5e650b41fa463c39614e7029
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-25 17:31:25 +0900

    feat: di 생성자 주입 lombok 어노테이션으로 변경

--- src/main/java/develop/grassserver/member/login/JwtUserService.java
+++ src/main/java/develop/grassserver/member/login/JwtUserService.java
@@ -5,20 +5,15 @@ import develop.grassserver.member.MemberRepository;
 import develop.grassserver.member.exception.ForbiddenException;
 import develop.grassserver.utils.jwt.JwtService;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class JwtUserService {
     private final MemberRepository memberRepository;
     private final JwtService jwtService;
-    private final MemberService memberService;
-
-    public JwtUserService(MemberRepository memberRepository, JwtService jwtService, MemberService memberService) {
-        this.memberRepository = memberRepository;
-        this.jwtService = jwtService;
-        this.memberService = memberService;
-    }
 
     @Transactional(readOnly = true)
     public String login(LoginRequest loginRequest) {
