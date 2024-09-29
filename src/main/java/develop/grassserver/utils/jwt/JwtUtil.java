d669f5f53b91b539a66bdf3385e20202789cfc85
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-30 00:54:39 +0900

    feat: redis에 리프레시 토큰 저장/조회/삭제 기능 구현

--- src/main/java/develop/grassserver/utils/jwt/JwtUtil.java
+++ src/main/java/develop/grassserver/utils/jwt/JwtUtil.java
@@ -9,6 +9,7 @@ public final class JwtUtil {
     public static final String ISSUER = "GrassServer";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final int TOKEN_BEGIN_INDEX = 7;
+    public static final Long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 24 * 30L; // 30일
 
     private JwtUtil() {
     }
