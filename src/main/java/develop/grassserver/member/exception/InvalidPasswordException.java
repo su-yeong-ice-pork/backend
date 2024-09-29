085ec00a03cf50636b0824a0bda466a8e5247630
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-30 02:25:43 +0900

    fix: http 에러 코드 수정

--- src/main/java/develop/grassserver/member/exception/InvalidPasswordException.java
+++ src/main/java/develop/grassserver/member/exception/InvalidPasswordException.java
@@ -7,7 +7,7 @@ public class InvalidPasswordException extends RuntimeException {
     private final String message = "로그인이 실패하였습니다.";
 
     public ApiUtils.ApiResult<?> body() {
-        return ApiUtils.error(HttpStatus.BAD_REQUEST, message);
+        return ApiUtils.error(HttpStatus.UNAUTHORIZED, message);
     }
 
     public HttpStatus status() {
