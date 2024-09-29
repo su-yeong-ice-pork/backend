f06bff48fc5346aa2db5ca3e2f5ffaf1b8a76f93
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-30 00:55:51 +0900

    feat: 리프레시 토큰, 액세스 토큰 유효시간 상수 선언

--- src/main/java/develop/grassserver/utils/jwt/JwtUtil.java
+++ src/main/java/develop/grassserver/utils/jwt/JwtUtil.java
@@ -9,7 +9,8 @@ public final class JwtUtil {
     public static final String ISSUER = "GrassServer";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final int TOKEN_BEGIN_INDEX = 7;
-    public static final Long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 24 * 30L; // 30일
+    public static final long REFRESH_TOKEN_EXPIRATION_TIME = 60 * 60 * 24 * 30L; // 30일
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 60 * 60 * 24;
 
     private JwtUtil() {
     }
