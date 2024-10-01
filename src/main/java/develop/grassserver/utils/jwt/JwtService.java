0fdf1bad3c2407d6cf23f5e97bb4ebd0e8d68789
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-02 01:42:51 +0900

    feat: refresh 토큰 구현

--- src/main/java/develop/grassserver/utils/jwt/JwtService.java
+++ src/main/java/develop/grassserver/utils/jwt/JwtService.java
@@ -10,6 +10,7 @@ import static develop.grassserver.utils.jwt.JwtUtil.TOKEN_PREFIX;
 
 import develop.grassserver.member.auth.RedisService;
 import develop.grassserver.member.auth.TokenDTO;
+import develop.grassserver.member.exception.ReauthenticationRequiredException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
@@ -50,19 +51,6 @@ public class JwtService {
         return claims.getSubject();
     }
 
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser()
-                    .verifyWith(SECRET_KEY)
-                    .build()
-                    .parseSignedClaims(removeBearerPrefix(token));
-            return true;
-        } catch (SecurityException | io.jsonwebtoken.MalformedJwtException | io.jsonwebtoken.ExpiredJwtException |
-                 io.jsonwebtoken.UnsupportedJwtException | IllegalArgumentException ex) {
-            return false;
-        }
-    }
-
     public String extractToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_PREFIX)) {
@@ -78,12 +66,16 @@ public class JwtService {
         return token.substring(TOKEN_BEGIN_INDEX);
     }
 
-    public void saveRefreshToken(String email, String refreshToken) {
-        redisService.saveRefreshToken(email, refreshToken);
+    public void saveRefreshToken(String code, String refreshToken) {
+        redisService.saveRefreshToken(code, refreshToken);
     }
 
-    public boolean validateRefreshToken(String email, String refreshToken) {
-        String savedRefreshToken = redisService.getRefreshToken(email);
-        return savedRefreshToken != null && savedRefreshToken.equals(refreshToken);
+    public TokenDTO renewTokens(String code) {
+        String savedRefreshToken = redisService.getRefreshToken(code);
+        if (savedRefreshToken == null) {
+            throw new ReauthenticationRequiredException();
+        }
+        String email = getEmailFromToken(savedRefreshToken);
+        return createAllToken(email);
     }
 }
