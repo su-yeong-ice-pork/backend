3c20634d578296c467daaa72f92a4361ea090aa9
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-03 23:14:53 +0900

    feat: memberid와 현재 날짜로 잔디 조회 기능 구현 - memberId로 잔디 조회 기능 삭제

--- src/main/java/develop/grassserver/grass/GrassRepository.java
+++ src/main/java/develop/grassserver/grass/GrassRepository.java
@@ -1,10 +1,12 @@
 package develop.grassserver.grass;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GrassRepository extends JpaRepository<Grass, Long> {
-    Optional<Grass> findByMemberId(Long memberId);
+    Optional<Grass> findByMemberIdAndCreatedAt(Long memberId, LocalDate createdAt);
+
 }
--- src/main/java/develop/grassserver/grass/GrassService.java
+++ src/main/java/develop/grassserver/grass/GrassService.java
@@ -5,6 +5,7 @@ import develop.grassserver.grass.dto.StudyTimeResponse;
 import develop.grassserver.grass.exception.MissingAttendanceException;
 import develop.grassserver.member.Member;
 import develop.grassserver.utils.duration.DurationUtils;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +14,20 @@ import org.springframework.stereotype.Service;
 public class GrassService {
     private final GrassRepository grassRepository;
 
-    public Grass findGrassByMemberId(Long memberId) {
-        return grassRepository.findByMemberId(memberId)
+    public Grass findTodayGrassByMemberId(Long memberId) {
+        return grassRepository.findByMemberIdAndCreatedAt(memberId, LocalDate.now())
                 .orElseThrow(MissingAttendanceException::new);
     }
 
     public StudyTimeResponse getStudyRecord(Member member) {
-        Grass grass = findGrassByMemberId(member.getId());
+        Grass grass = findTodayGrassByMemberId(member.getId());
         String todayStudyTime = DurationUtils.formatDuration(grass.getStudyTime());
         String totalStudyTime = DurationUtils.formatDuration(member.getStudyRecord().getTotalStudyTime());
         return new StudyTimeResponse(todayStudyTime, totalStudyTime);
     }
 
     public void updateStudyRecord(Member member, StudyTimeRequest request) {
-        Grass grass = findGrassByMemberId(member.getId());
+        Grass grass = findTodayGrassByMemberId(member.getId());
         grass.updateStudyTime(DurationUtils.parseDuration(request.todayStudyTime()));
         grassRepository.save(grass);
     }
