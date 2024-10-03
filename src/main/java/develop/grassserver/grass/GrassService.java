418237bf212e1ecd8988ff49f34c39ebb98dea51
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-04 00:02:48 +0900

    feat: service 단에서 잔디 연간/월간 조회 함수 구현

--- src/main/java/develop/grassserver/grass/GrassService.java
+++ src/main/java/develop/grassserver/grass/GrassService.java
@@ -6,6 +6,7 @@ import develop.grassserver.grass.exception.MissingAttendanceException;
 import develop.grassserver.member.Member;
 import develop.grassserver.utils.duration.DurationUtils;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,14 @@ public class GrassService {
     public Grass findTodayGrassByMemberId(Long memberId) {
         return grassRepository.findByMemberIdAndCreatedAt(memberId, LocalDate.now())
                 .orElseThrow(MissingAttendanceException::new);
+    }
+
+    public List<Grass> findYearlyGrassByMemberId(Member member, int year) {
+        return grassRepository.findByMemberAndYear(member, year);
+    }
+
+    public List<Grass> findMonthlyGrassByMemberId(Member member, int year, int month) {
+        return grassRepository.findByMemberAndYearAndMonth(member, year, month);
     }
 
     public StudyTimeResponse getStudyRecord(Member member) {
