bcc7727a137b2cf027e466c523bd8bded7888db8
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-03 21:00:01 +0900

    feat: 공부시간 저장 request/response 타입 변경

--- src/main/java/develop/grassserver/grass/dto/StudyTimeRequest.java
+++ src/main/java/develop/grassserver/grass/dto/StudyTimeRequest.java
@@ -2,12 +2,11 @@ package develop.grassserver.grass.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import java.time.LocalTime;
 
 @Schema(description = "공부시간 저장 요청 DTO")
 public record StudyTimeRequest(
         @Schema(description = "오늘 공부시간", example = "08:32:11")
         @NotBlank(message = "공부시간은 필수 항목입니다.")
-        LocalTime todayStudyTime
+        String todayStudyTime
 ) {
 }
--- src/main/java/develop/grassserver/grass/dto/StudyTimeResponse.java
+++ src/main/java/develop/grassserver/grass/dto/StudyTimeResponse.java
@@ -1,9 +1,7 @@
 package develop.grassserver.grass.dto;
 
-import java.time.LocalTime;
-
 public record StudyTimeResponse(
-        LocalTime todayStudyTime,
-        LocalTime totalStudyTime
+        String todayStudyTime,
+        String totalStudyTime
 ) {
 }
