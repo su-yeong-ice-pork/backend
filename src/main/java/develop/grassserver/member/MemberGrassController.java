9559276cd796cedc06cf97d83ff3ab62f3713003
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-03 23:20:22 +0900

    feat: member steak 정보 조회 api 작성

--- src/main/java/develop/grassserver/member/MemberGrassController.java
+++ src/main/java/develop/grassserver/member/MemberGrassController.java
@@ -1,14 +1,21 @@
 package develop.grassserver.member;
 
+import develop.grassserver.member.dto.MemberStreakResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "멤버 스트릭 관련 조회 APIs", description = "현재 스트릭, 최장 스트릭, 연간 잔디 등을 담당하는 APIs")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/v1/members/{id}/grass")
 public class MemberGrassController {
     private final MemberGrassService memberGrassService;
+
+    ResponseEntity<MemberStreakResponse> getMemberStreak(@PathVariable("id") Long memberId) {
+        return ResponseEntity.ok(memberGrassService.getMemberStreak(memberId));
+    }
 }
