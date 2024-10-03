d883119da716f9c32b246366e1f8a093de7a27d9
MinseokGo <rhalstjr1999@naver.com>
2024-10-04 01:04:56 +0900

    feat: 배너 엔티티 등록

---
+++ src/main/java/develop/grassserver/member/profile/banner/BannerRepository.java
@@ -0,0 +1,6 @@
+package develop.grassserver.member.profile.banner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+}
