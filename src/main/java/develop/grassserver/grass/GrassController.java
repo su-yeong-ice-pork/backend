b66d16ef2e337735de5d50d5d39e4e0caa3546e9
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-03 21:21:29 +0900

    docs: 공부시간 관련 api swagger 명세

--- src/main/java/develop/grassserver/grass/GrassController.java
+++ src/main/java/develop/grassserver/grass/GrassController.java
@@ -6,6 +6,9 @@ import develop.grassserver.member.Member;
 import develop.grassserver.utils.ApiUtils;
 import develop.grassserver.utils.ApiUtils.ApiResult;
 import develop.grassserver.utils.annotation.LoginMember;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +25,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class GrassController {
     private final GrassService grassService;
 
+    @Operation(summary = "공부시간 조회 API", description = "기록장 타이머 공부시간 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공. 응답 에러 코드는 무시하셈"),
+            @ApiResponse(responseCode = "401", description = "인증 실패, 재로그인 필요"),
+            @ApiResponse(responseCode = "404", description = "출석 기록이 없음")
+    })
     @GetMapping("study-time")
     public ResponseEntity<ApiResult<StudyTimeResponse>> getStudyRecord(@LoginMember Member member) {
         StudyTimeResponse studyRecord = grassService.getStudyRecord(member);
         return ResponseEntity.ok(ApiUtils.success(studyRecord));
     }
 
+    @Operation(summary = "공부시간 저장 API", description = "기록장 타이머 공부시간 저장 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "저장 성공. 응답 에러 코드는 무시하셈"),
+            @ApiResponse(responseCode = "401", description = "인증 실패, 재로그인 필요"),
+            @ApiResponse(responseCode = "404", description = "출석 기록이 없음")
+    })
     @PatchMapping("study-time")
     public ResponseEntity<ApiResult<String>> updateStudyRecord(@LoginMember Member member,
                                                                @Valid StudyTimeRequest request) {
