956ac7e384b0b124c9d538c0c1e6bc307e0fb982
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-04 00:16:53 +0900

    feat: 잔디 연간 조회 api 작성

--- src/main/java/develop/grassserver/member/memberGrass/MemberGrassController.java
+++ src/main/java/develop/grassserver/member/memberGrass/MemberGrassController.java
@@ -1,21 +1,33 @@
 package develop.grassserver.member.memberGrass;
 
 import develop.grassserver.member.memberGrass.dto.MemberStreakResponse;
+import develop.grassserver.member.memberGrass.dto.YearlyTotalGrassResponse;
+import develop.grassserver.utils.ApiUtils;
+import develop.grassserver.utils.ApiUtils.ApiResult;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "멤버 스트릭 관련 조회 APIs", description = "현재 스트릭, 최장 스트릭, 연간 잔디 등을 담당하는 APIs")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/members/{id}/grass")
+@RequestMapping("/api/v1/members/{id}")
 public class MemberGrassController {
     private final MemberGrassService memberGrassService;
 
-    ResponseEntity<MemberStreakResponse> getMemberStreak(@PathVariable("id") Long memberId) {
-        return ResponseEntity.ok(memberGrassService.getMemberStreak(memberId));
+    @GetMapping("/record")
+    public ResponseEntity<ApiResult<MemberStreakResponse>> getMemberStreak(@PathVariable("id") Long memberId) {
+        return ResponseEntity.ok(ApiUtils.success(memberGrassService.getMemberStreak(memberId)));
+    }
+
+    @GetMapping("/grass/yearly")
+    public ResponseEntity<ApiResult<YearlyTotalGrassResponse>> getYearlyTotalGrass(@PathVariable("id") Long memberId,
+                                                                                   @RequestParam("year") int year) {
+        return ResponseEntity.ok(ApiUtils.success(memberGrassService.getYearlyGrass(memberId, year)));
     }
 }
--- src/main/java/develop/grassserver/member/memberGrass/MemberGrassService.java
+++ src/main/java/develop/grassserver/member/memberGrass/MemberGrassService.java
@@ -5,6 +5,8 @@ import develop.grassserver.grass.GrassService;
 import develop.grassserver.member.Member;
 import develop.grassserver.member.MemberService;
 import develop.grassserver.member.memberGrass.dto.MemberStreakResponse;
+import develop.grassserver.member.memberGrass.dto.YearlyGrassResponse;
+import develop.grassserver.member.memberGrass.dto.YearlyTotalGrassResponse;
 import develop.grassserver.utils.duration.DurationUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,20 @@ public class MemberGrassService {
                 member.getStudyRecord().getTopStreak(),
                 DurationUtils.formatDuration(member.getStudyRecord().getTotalStudyTime())
         );
+    }
 
+    public YearlyTotalGrassResponse getYearlyGrass(Long memberId, int year) {
+        Member member = memberService.findMemberById(memberId);
+        return new YearlyTotalGrassResponse(
+                year,
+                grassService.findYearlyGrassByMemberId(member, year).stream()
+                        .map(grass -> new YearlyGrassResponse(
+                                grass.getId(),
+                                grass.getCreatedAt().getMonthValue(),
+                                grass.getCreatedAt().getDayOfMonth(),
+                                DurationUtils.formatDuration(grass.getStudyTime())
+                        ))
+                        .toList()
+        );
     }
 }
