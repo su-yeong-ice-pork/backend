ed0980fdf55c270301073f91e79f297708266e9a
MinseokGo <96585636+MinseokGo@users.noreply.github.com>
2024-10-13 12:47:21 +0900

    Revert "Revert "Revert "Revert "chore: 스프링 부트 앱 Timezone 설정""""

--- src/main/java/develop/grassserver/GrassServerApplication.java
+++ src/main/java/develop/grassserver/GrassServerApplication.java
@@ -1,7 +1,5 @@
 package develop.grassserver;
 
-import jakarta.annotation.PostConstruct;
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,10 +10,5 @@ public class GrassServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GrassServerApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
