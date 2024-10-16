c11c89616cf7e09c83fa2a7220ba94648a94fa42
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-17 01:46:24 +0900

    feat: 마이페이지 전체 스트릭 조회 response 수정 - 처음 시작 날짜 추가

--- src/main/java/develop/grassserver/member/memberGrass/dto/MemberTotalStreakResponse.java
+++ src/main/java/develop/grassserver/member/memberGrass/dto/MemberTotalStreakResponse.java
@@ -1,6 +1,7 @@
 package develop.grassserver.member.memberGrass.dto;
 
 public record MemberTotalStreakResponse(
+        String createdDate,
         long totalStreak,
         int totalStudyTime
 ) {
