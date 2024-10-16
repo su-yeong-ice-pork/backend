81a3e0b2084cf1b83fbf8c23a2d0e04ebfb0440c
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-17 01:30:38 +0900

    feat: 마이페이지 전체 스트릭 조회 api 작성 - swagger 명세 추가

--- src/main/java/develop/grassserver/member/memberGrass/MemberPageController.java
+++ src/main/java/develop/grassserver/member/memberGrass/MemberPageController.java
@@ -1,7 +1,17 @@
 package develop.grassserver.member.memberGrass;
 
+import develop.grassserver.member.Member;
+import develop.grassserver.member.memberGrass.dto.MemberTotalStreakResponse;
+import develop.grassserver.utils.ApiUtils;
+import develop.grassserver.utils.ApiUtils.ApiResult;
+import develop.grassserver.utils.annotation.LoginMember;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberPageController {
     private final MemberGrassService memberGrassService;
 
-
+    @Operation(summary = "마이페이지 전체 스트릭 조회 API", description = "앱 사용 일수, 총 공부시간 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공. 응답 에러 코드는 무시하셈"),
+            @ApiResponse(responseCode = "401", description = "인증 실패, 재로그인 필요"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 사용자")
+    })
+    @GetMapping("/record")
+    public ResponseEntity<ApiResult<MemberTotalStreakResponse>> getMemberTotalStreak(@LoginMember Member member) {
+        return ResponseEntity.ok(ApiUtils.success(memberGrassService.getMemberTotalStreak(member.getId())));
+    }
 }
