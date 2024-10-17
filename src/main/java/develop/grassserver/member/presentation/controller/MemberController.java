6070ecb309ab46177f84416ad780a8329defd60b
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-18 01:10:48 +0900

    refactor: 인증 관련 api를 책임지는 controller 분리

--- src/main/java/develop/grassserver/member/presentation/controller/MemberController.java
+++ src/main/java/develop/grassserver/member/presentation/controller/MemberController.java
@@ -1,15 +1,9 @@
 package develop.grassserver.member.presentation.controller;
 
-import develop.grassserver.auth.application.service.AuthService;
-import develop.grassserver.auth.presentation.dto.LoginRequest;
-import develop.grassserver.auth.presentation.dto.RefreshTokenDTO;
-import develop.grassserver.auth.presentation.dto.TokenDTO;
-import develop.grassserver.common.annotation.LoginMember;
 import develop.grassserver.common.security.CustomUserDetails;
 import develop.grassserver.common.utils.ApiUtils;
 import develop.grassserver.common.utils.ApiUtils.ApiResult;
 import develop.grassserver.member.application.service.MemberService;
-import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.member.presentation.dto.ChangePasswordRequest;
 import develop.grassserver.member.presentation.dto.MemberAuthRequest;
 import develop.grassserver.member.presentation.dto.MemberJoinRequest;
@@ -37,7 +31,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
-    private final AuthService authService;
 
     @Operation(summary = "내정보 조회 API", description = "내정보 조회 시 사용되는 API")
     @ApiResponses(value = {
@@ -53,40 +46,6 @@ public class MemberController {
                 .body(ApiUtils.success(response));
     }
 
-    @Operation(summary = "로그인 API", description = "멤버 로그인 시 사용되는 API")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "로그인 성공. 응답 에러 코드는 무시하셈"),
-            @ApiResponse(responseCode = "401", description = "로그인 실패"),
-            @ApiResponse(responseCode = "404", description = "해당 멤버를 찾을 수 없음")
-    })
-    @PostMapping("/login")
-    public ResponseEntity<ApiResult<TokenDTO>> login(@Valid @RequestBody LoginRequest loginRequest) {
-        TokenDTO token = authService.login(loginRequest);
-        return ResponseEntity.ok()
-                .header("Authorization", token.accessToken())
-                .body(ApiUtils.success(token));
-    }
-
-    @Operation(summary = "자동 로그인 API", description = "멤버 자동 로그인 시 사용되는 API")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "자동 로그인 성공. 응답 에러 코드는 무시하셈"),
-            @ApiResponse(responseCode = "401", description = "자동 로그인 실패, 재로그인 필요"),
-            @ApiResponse(responseCode = "404", description = "해당 멤버를 찾을 수 없음")
-    })
-    @PostMapping("/auto-login")
-    public ResponseEntity<ApiResult<String>> autoLogin(@Valid @RequestBody RefreshTokenDTO refreshTokenDTO) {
-        TokenDTO token = authService.autoLogin(refreshTokenDTO);
-        return ResponseEntity.ok()
-                .header("Authorization", token.accessToken())
-                .body(ApiUtils.success());
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity<ApiResult<String>> logout(@LoginMember Member member) {
-        // 로그인한 사용자 검증 필요
-        return ResponseEntity.ok()
-                .body(ApiUtils.success());
-    }
 
     @Operation(summary = "회원가입 API", description = "멤버 회원가입 시 사용되는 API")
     @ApiResponses(value = {
