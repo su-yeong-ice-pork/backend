b7a4f49d69b2487cd4de052bf13bce7c5d4d3e93
MinseokGo <rhalstjr1999@naver.com>
2024-10-17 23:35:38 +0900

    refactor: 디폴트 메시지 책임 이동

--- src/main/java/develop/grassserver/profile/domain/entity/Profile.java
+++ src/main/java/develop/grassserver/profile/domain/entity/Profile.java
@@ -16,6 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Profile extends BaseEntity {
 
+    public static final String DEFAULT_PROFILE_IMAGE =
+            "https://grass-bucket.s3.us-east-2.amazonaws.com/%E1%84%91%E1%85%B3%E1%84%85%E1%85%A9%E1%84%91%E1%85%B5%E1%86%AF%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B5.png";
+    public static final String DEFAULT_BANNER_IMAGE = "https://grass-bucket.s3.us-east-2.amazonaws.com/bannerImage1.png";
+    public static final String DEFAULT_PROFILE_MESSAGE = "오늘 하루도 화이팅!";
+    public static final String DEFAULT_TITLE_NAME = "초심자";
+
     @Column(length = 1024, nullable = false)
     private String image;
 
