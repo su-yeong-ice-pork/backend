fe87e8fbe434ff61949eeb14d76b5cda9ba2fbba
MinseokGo <rhalstjr1999@naver.com>
2024-11-01 15:43:24 +0900

    feat: 회원 탈퇴 시 비밀번호를 검증하는 기능

--- src/main/java/develop/grassserver/member/presentation/controller/MemberController.java
+++ src/main/java/develop/grassserver/member/presentation/controller/MemberController.java
@@ -6,6 +6,7 @@ import develop.grassserver.common.utils.ApiUtils.ApiResult;
 import develop.grassserver.member.application.service.MemberService;
 import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.member.presentation.dto.ChangePasswordRequest;
+import develop.grassserver.member.presentation.dto.DeleteMemberRequest;
 import develop.grassserver.member.presentation.dto.MemberJoinRequest;
 import develop.grassserver.member.presentation.dto.MemberProfileResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,7 +20,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -80,12 +80,12 @@ public class MemberController {
             @ApiResponse(responseCode = "401", description = "인증 실패"),
             @ApiResponse(responseCode = "404", description = "정보를 찾을 수 없음"),
     })
-    @DeleteMapping("/{id}")
+    @DeleteMapping
     public ResponseEntity<ApiResult<String>> deleteMember(
-            @PathVariable Long id,
-            @LoginMember Member member
+            @LoginMember Member member,
+            @Valid @RequestBody DeleteMemberRequest request
     ) {
-        memberService.deleteMember(id, member);
+        memberService.deleteMember(request, member);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiUtils.success());
     }
