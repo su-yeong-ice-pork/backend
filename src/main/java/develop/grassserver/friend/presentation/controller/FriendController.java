72bd090b10ad9397d5df7a742e859f3fffd4eb67
MinseokGo <rhalstjr1999@naver.com>
2024-11-18 16:01:56 +0900

    feat: 친구 추가 기능 스웨거 등록

--- src/main/java/develop/grassserver/friend/presentation/controller/FriendController.java
+++ src/main/java/develop/grassserver/friend/presentation/controller/FriendController.java
@@ -6,6 +6,9 @@ import develop.grassserver.common.utils.ApiUtils.ApiResult;
 import develop.grassserver.friend.application.service.FriendService;
 import develop.grassserver.friend.presentation.dto.RequestFriendRequest;
 import develop.grassserver.member.domain.entity.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,13 @@ public class FriendController {
 
     private final FriendService friendService;
 
+    @Operation(summary = "친구 추가 API", description = "친구 추가 시 사용되는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "친구 추가 성공. 응답 에러 코드는 무시하셈"),
+            @ApiResponse(responseCode = "400", description = "친구 추가 실패"),
+            @ApiResponse(responseCode = "404", description = "상대 멤버 정보를 찾을 수 없음"),
+            @ApiResponse(responseCode = "409", description = "이미 등록된 친구")
+    })
     @PostMapping
     public ResponseEntity<ApiResult<String>> requestFriend(
             @LoginMember Member member,
