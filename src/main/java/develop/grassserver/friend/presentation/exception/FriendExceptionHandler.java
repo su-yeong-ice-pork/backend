dc24538c66e016a66ba39fdeff7ffdc9eebd10cc
MinseokGo <rhalstjr1999@naver.com>
2024-11-19 00:49:09 +0900

    feat: 이미 친구 요청을 보낸 상태 예외 처리

--- src/main/java/develop/grassserver/friend/presentation/exception/FriendExceptionHandler.java
+++ src/main/java/develop/grassserver/friend/presentation/exception/FriendExceptionHandler.java
@@ -1,6 +1,7 @@
 package develop.grassserver.friend.presentation.exception;
 
 import develop.grassserver.common.utils.ApiUtils;
+import develop.grassserver.friend.application.exception.AlreadyFriendRequestException;
 import develop.grassserver.friend.application.exception.ExistFriendRelationException;
 import develop.grassserver.friend.application.exception.NotExistFriendRelationException;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,13 @@ public class FriendExceptionHandler {
     @ExceptionHandler(NotExistFriendRelationException.class)
     public ResponseEntity<ApiUtils.ApiResult<?>> notExistFriendRelationException(
             NotExistFriendRelationException exception
+    ) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+
+    @ExceptionHandler(AlreadyFriendRequestException.class)
+    public ResponseEntity<ApiUtils.ApiResult<?>> alreadyFriendRequestException(
+            AlreadyFriendRequestException exception
     ) {
         return new ResponseEntity<>(exception.body(), exception.status());
     }
