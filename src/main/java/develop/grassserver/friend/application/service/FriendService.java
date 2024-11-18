9b541ae7d9249316371981cb70bd9eca751d8d0b
MinseokGo <rhalstjr1999@naver.com>
2024-11-19 01:14:04 +0900

    fix: 읽기 전용 트랜잭션 선언

--- src/main/java/develop/grassserver/friend/application/service/FriendService.java
+++ src/main/java/develop/grassserver/friend/application/service/FriendService.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FriendService {
 
     private final MemberService memberService;
