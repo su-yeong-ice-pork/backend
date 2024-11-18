bc0959e795d9cf3c1846d4119bf6e6f80c57e064
MinseokGo <rhalstjr1999@naver.com>
2024-11-18 23:33:58 +0900

    feat: 오늘 특정 유저에게 보낸 알림 횟수 조회 쿼리 메서드 작성

--- src/main/java/develop/grassserver/notification/infrastructure/repository/EmojiNotificationRepository.java
+++ src/main/java/develop/grassserver/notification/infrastructure/repository/EmojiNotificationRepository.java
@@ -1,7 +1,18 @@
 package develop.grassserver.notification.infrastructure.repository;
 
+import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.notification.domain.entity.EmojiNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmojiNotificationRepository extends JpaRepository<EmojiNotification, Long> {
+
+    @Query("select count(e) "
+            + "from EmojiNotification e "
+            + "where (e.sender = :sender and e.receiver = :receiver) "
+            + "and (date(e.createdAt) = current_date)")
+    long findAllBySenderAndReceiverAndToday(
+            @Param("sender") Member sender,
+            @Param("receiver") Member receiver);
 }
