426c44607337288df0d941ff3fbea392de53eea5
MinseokGo <rhalstjr1999@naver.com>
2024-11-18 22:41:28 +0900

    feat: 응원 스티커 보내기 컨트롤러 메서드 작성

--- src/main/java/develop/grassserver/friend/presentation/controller/FriendController.java
+++ src/main/java/develop/grassserver/friend/presentation/controller/FriendController.java
@@ -5,10 +5,12 @@ import develop.grassserver.common.utils.ApiUtils;
 import develop.grassserver.common.utils.ApiUtils.ApiResult;
 import develop.grassserver.friend.application.service.FriendService;
 import develop.grassserver.friend.presentation.dto.RequestFriendRequest;
+import develop.grassserver.friend.presentation.dto.SendCheerUpEmojiRequest;
 import develop.grassserver.member.domain.entity.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +32,7 @@ public class FriendController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "친구 추가 성공. 응답 에러 코드는 무시하셈"),
             @ApiResponse(responseCode = "400", description = "친구 추가 실패"),
+            @ApiResponse(responseCode = "401", description = "인증 실패"),
             @ApiResponse(responseCode = "404", description = "상대 멤버 정보를 찾을 수 없음"),
             @ApiResponse(responseCode = "409", description = "이미 등록된 친구")
     })
@@ -45,8 +48,9 @@ public class FriendController {
 
     @Operation(summary = "친구 떠나기 API", description = "친구 떠나기 시 사용되는 API")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "친구 떠나기 성공. 응답 에러 코드는 무시하셈"),
+            @ApiResponse(responseCode = "200", description = "친구 떠나기 성공. 응답 에러 코드는 무시하셈"),
             @ApiResponse(responseCode = "400", description = "친구 관계가 아니므로 친구 떠나기 실패"),
+            @ApiResponse(responseCode = "401", description = "인증 실패"),
             @ApiResponse(responseCode = "404", description = "상대 멤버 정보를 찾을 수 없음")
     })
     @DeleteMapping("/{id}")
@@ -58,4 +62,24 @@ public class FriendController {
         return ResponseEntity.ok()
                 .body(ApiUtils.success());
     }
+
+    @Operation(summary = "응원 스티거 보내기 API", description = "응원 스티거 보내기 시 사용되는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "응원 스티거 보내기 성공. 응답 에러 코드는 무시하셈"),
+            @ApiResponse(responseCode = "400", description = "친구 관계가 아니므로 응원 스티거 보내기 실패"),
+            @ApiResponse(responseCode = "400", description = "응원 스티커 보내기 횟수 초과"),
+            @ApiResponse(responseCode = "401", description = "인증 실패"),
+            @ApiResponse(responseCode = "404", description = "상대 멤버 정보를 찾을 수 없음")
+    })
+    @PostMapping("/{id}/emoji")
+    public ResponseEntity<ApiResult<String>> sendCheerUpEmoji(
+            @PathVariable Long id,
+            @LoginMember Member member,
+            @Valid @RequestBody SendCheerUpEmojiRequest request
+    ) {
+        friendService.sendCheerUpEmoji(id, member, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiUtils.success());
+    }
+
 }
