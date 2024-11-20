21be24dc777cdda120ad201f49d19a55659fb8d4
MinseokGo <rhalstjr1999@naver.com>
2024-11-21 02:14:54 +0900

    feat: 생성자 추가

--- src/main/java/develop/grassserver/requestNotification/domain/entity/FriendRequestNotification.java
+++ src/main/java/develop/grassserver/requestNotification/domain/entity/FriendRequestNotification.java
@@ -1,18 +1,25 @@
 package develop.grassserver.requestNotification.domain.entity;
 
 import develop.grassserver.friend.domain.entity.Friend;
+import develop.grassserver.member.domain.entity.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToOne;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FriendRequestNotification extends RequestNotification {
 
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     private Friend friend;
+
+    public FriendRequestNotification(Member sender, Member receiver, Friend friend) {
+        super(sender, receiver);
+        this.friend = friend;
+    }
 }
