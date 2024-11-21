8014a41e76fa49f6ef1633363fba644714b6518f
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 16:31:07 +0900

    feat: createStudyRequest에 엔티티 맵퍼 함수 제거

--- src/main/java/develop/grassserver/study/presentation/dto/CreateStudyRequest.java
+++ src/main/java/develop/grassserver/study/presentation/dto/CreateStudyRequest.java
@@ -1,19 +1,8 @@
 package develop.grassserver.study.presentation.dto;
 
-import develop.grassserver.study.domain.entity.Study;
-
 public record CreateStudyRequest(
         String name,
         String goalMessage,
         long goalTime
 ) {
-
-    public static Study from(CreateStudyRequest request) {
-        return Study.builder()
-                .name(request.name())
-                .goalMessage(request.goalMessage())
-                .goalTime(request.goalTime())
-                .build();
-
-    }
 }
