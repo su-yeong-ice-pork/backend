3d3367883e66370146ae529daeecb18a94e362f2
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 18:10:04 +0900

    feat: 고정 스터디 상세 조회 response에서 엔티티 매핑 함수 구현 - 엔티티 -> response

--- src/main/java/develop/grassserver/study/presentation/dto/StudyDetailResponse.java
+++ src/main/java/develop/grassserver/study/presentation/dto/StudyDetailResponse.java
@@ -1,5 +1,9 @@
 package develop.grassserver.study.presentation.dto;
 
+import static develop.grassserver.common.utils.duration.DurationUtils.formatHourDuration;
+
+import develop.grassserver.study.domain.entity.Study;
+
 public record StudyDetailResponse(
         Long id,
         String studyName,
@@ -8,4 +12,14 @@ public record StudyDetailResponse(
         Long totalStudyTime,
         String inviteCode
 ) {
+    public static StudyDetailResponse from(Study study) {
+        return new StudyDetailResponse(
+                study.getId(),
+                study.getName(),
+                study.getGoalMessage(),
+                study.getGoalTime(),
+                formatHourDuration(study.getTotalStudyTime()),
+                study.getInviteCode()
+        );
+    }
 }
