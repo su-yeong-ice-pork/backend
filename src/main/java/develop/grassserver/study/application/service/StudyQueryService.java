c78d807ac59b329ffb288f3be41e9b423d8389e6
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-22 04:24:58 +0900

    feat: 고정 스터디 멤버 조회 기능 구현

--- src/main/java/develop/grassserver/study/application/service/StudyQueryService.java
+++ src/main/java/develop/grassserver/study/application/service/StudyQueryService.java
@@ -1,16 +1,24 @@
 package develop.grassserver.study.application.service;
 
 import develop.grassserver.common.utils.duration.DurationUtils;
+import develop.grassserver.grass.application.dto.MemberStudyInfoDTO;
+import develop.grassserver.grass.application.service.MemberStudyInfoService;
 import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.study.application.exception.NotAStudyMemberException;
 import develop.grassserver.study.domain.entity.Study;
+import develop.grassserver.study.domain.entity.StudyMember;
+import develop.grassserver.study.domain.entity.StudyRole;
 import develop.grassserver.study.infrastructure.repository.StudyMemberRepository;
 import develop.grassserver.study.infrastructure.repository.StudyRepository;
+import develop.grassserver.study.presentation.dto.FindAllStudyMembersResponse;
 import develop.grassserver.study.presentation.dto.FindAllStudyResponse;
 import develop.grassserver.study.presentation.dto.StudyDetailResponse;
 import develop.grassserver.study.presentation.dto.StudySummaryResponse;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +28,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class StudyQueryService {
+
+    private final MemberStudyInfoService memberStudyInfoService;
 
     private final StudyRepository studyRepository;
     private final StudyMemberRepository studyMemberRepository;
@@ -56,5 +66,24 @@ public class StudyQueryService {
                 .collect(Collectors.toList());
 
         return new FindAllStudyResponse(regularStudies);
+    }
+
+    public FindAllStudyMembersResponse getAllStudyMember(Member member, Long studyId) {
+        validStudyMember(member, studyId);
+
+        List<StudyMember> studyMembers = studyMemberRepository.findAllByStudyId(studyId);
+
+        List<Long> studyMemberIds = new ArrayList<>();
+        Map<Long, StudyRole> roles = new HashMap<>();
+
+        studyMembers.forEach(studyMember -> {
+            Long id = studyMember.getMember().getId();
+            studyMemberIds.add(id);
+            roles.put(id, studyMember.getRole());
+        });
+
+        MemberStudyInfoDTO studyInfo = memberStudyInfoService.getMemberStudyInfo(studyMemberIds);
+
+        return FindAllStudyMembersResponse.from(studyInfo, roles);
     }
 }
