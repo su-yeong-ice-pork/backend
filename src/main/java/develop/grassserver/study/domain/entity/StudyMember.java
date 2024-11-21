df040f6fc509a580e0f7d2efc88fa5f87bce2864
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 09:59:43 +0900

    feat: studyMember 엔티티 작성

---
+++ src/main/java/develop/grassserver/study/domain/entity/StudyMember.java
@@ -0,0 +1,40 @@
+package develop.grassserver.study.domain.entity;
+
+import develop.grassserver.common.BaseEntity;
+import develop.grassserver.member.domain.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE study SET status = false WHERE id = ?")
+@SQLRestriction("status = true")
+public class StudyMember extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_id")
+    private Study study;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @ColumnDefault("'" + StudyRole.DEFAULT + "'")
+    @Builder.Default
+    private StudyRole role = StudyRole.MEMBER;
+}
