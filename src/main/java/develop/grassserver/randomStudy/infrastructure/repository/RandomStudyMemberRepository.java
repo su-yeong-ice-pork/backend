9627e88d2958041cd87c747af2b5c04a78eddb2c
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 17:58:09 +0900

    chore: 불필요한 repository 함수 삭제

--- src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyMemberRepository.java
+++ src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyMemberRepository.java
@@ -1,13 +1,10 @@
 package develop.grassserver.randomStudy.infrastructure.repository;
 
 import develop.grassserver.randomStudy.domain.entity.RandomStudyMember;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RandomStudyMemberRepository extends JpaRepository<RandomStudyMember, Long> {
-    List<RandomStudyMember> findByRandomStudyId(Long randomStudyId);
-
     boolean existsByMemberIdAndRandomStudyId(Long memberId, Long randomStudyId);
 }
