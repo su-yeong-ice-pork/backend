c60af22a6e50e1a35533400e7d45c9cfe534f2f1
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-22 03:17:11 +0900

    feat: 랜덤 스터디 구성원 전체 조회 기능 구현

--- src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyService.java
+++ src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyService.java
@@ -1,12 +1,17 @@
 package develop.grassserver.randomStudy.application.service;
 
+import develop.grassserver.grass.application.dto.MemberStudyInfoDTO;
+import develop.grassserver.grass.application.service.MemberStudyInfoService;
 import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.randomStudy.application.exception.NotARandomStudyMemberException;
 import develop.grassserver.randomStudy.domain.entity.RandomStudy;
+import develop.grassserver.randomStudy.domain.entity.RandomStudyMember;
 import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyMemberRepository;
 import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyRepository;
+import develop.grassserver.randomStudy.presentation.dto.FindAllRandomStudyMembersResponse;
 import develop.grassserver.randomStudy.presentation.dto.RandomStudyDetailResponse;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,17 +23,40 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RandomStudyService {
 
+    private final MemberStudyInfoService memberStudyInfoService;
+
     private final RandomStudyRepository randomStudyRepository;
     private final RandomStudyMemberRepository randomStudyMemberRepository;
 
     public RandomStudyDetailResponse getRandomStudyDetail(Member member, Long studyId) {
-        if (!randomStudyMemberRepository.existsByMemberIdAndRandomStudyId(member.getId(), studyId)) {
-            throw new NotARandomStudyMemberException();
-        }
+        validStudyMember(member, studyId);
 
         RandomStudy randomStudy = randomStudyRepository.findById(studyId)
                 .orElseThrow(() -> new EntityNotFoundException("해당하는 스터디를 찾을 수 없습니다."));
 
         return RandomStudyDetailResponse.from(randomStudy);
+    }
+
+    private void validStudyMember(Member member, Long studyId) {
+        if (!randomStudyMemberRepository.existsByMemberIdAndRandomStudyId(member.getId(), studyId)) {
+            throw new NotARandomStudyMemberException();
+        }
+    }
+
+    public FindAllRandomStudyMembersResponse getAllRandomStudyMember(Member member, Long studyId) {
+        validStudyMember(member, studyId);
+
+        List<RandomStudyMember> studyMembers = randomStudyMemberRepository.findAllByRandomStudyId(studyId);
+        List<Long> studyMemberIds = getStudyMemberIds(studyMembers);
+
+        MemberStudyInfoDTO studyInfo = memberStudyInfoService.getMemberStudyInfo(studyMemberIds);
+
+        return FindAllRandomStudyMembersResponse.from(studyInfo);
+    }
+
+    private List<Long> getStudyMemberIds(List<RandomStudyMember> studyMembers) {
+        return studyMembers.stream()
+                .map(studyMember -> studyMember.getMember().getId())
+                .toList();
     }
 }
