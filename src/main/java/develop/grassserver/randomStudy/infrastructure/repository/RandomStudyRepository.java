31b0fa1486f01b75ff1f70ef2b7ccc9988b03c7a
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 10:44:34 +0900

    chore: repository 이름 변경

--- src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyRepository.java
+++ src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyRepository.java
@@ -3,5 +3,5 @@ package develop.grassserver.randomStudy.infrastructure.repository;
 import develop.grassserver.randomStudy.domain.entity.RandomStudy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RandomServiceRepository extends JpaRepository<RandomStudy, Long> {
+public interface RandomStudyRepository extends JpaRepository<RandomStudy, Long> {
 }
