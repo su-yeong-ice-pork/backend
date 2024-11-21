efc23c9a2ae3ee146a3cdbd6071aed7b125a31eb
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-22 00:31:00 +0900

    feat: 초대 코드로 study 객체 찾기

--- src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
+++ src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
@@ -2,6 +2,7 @@ package develop.grassserver.study.infrastructure.repository;
 
 import develop.grassserver.study.domain.entity.Study;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +16,6 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
             "WHERE sm.status = TRUE AND s.status = TRUE AND sm.member.id = :memberId " +
             "GROUP BY s")
     List<Object[]> findStudiesWithMemberCountByMemberId(@Param("memberId") Long memberId);
+
+    Optional<Study> findStudyByInviteCode(String inviteCode);
 }
