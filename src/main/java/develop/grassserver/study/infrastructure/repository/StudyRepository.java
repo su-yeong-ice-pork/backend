9426a7f2045c5bbf91dec17edf907f3c70d71713
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 23:09:48 +0900

    feat: db에서 memberId로 고정 스터디 전체 조회 기능 구현

--- src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
+++ src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
@@ -1,8 +1,14 @@
 package develop.grassserver.study.infrastructure.repository;
 
 import develop.grassserver.study.domain.entity.Study;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
     boolean existsByInviteCode(String inviteCode);
+
+    @Query("SELECT sm.study FROM StudyMember sm WHERE sm.member.id = :memberId AND sm.status = TRUE")
+    List<Study> findAllStudiesByMemberId(@Param("memberId") Long memberId);
 }
