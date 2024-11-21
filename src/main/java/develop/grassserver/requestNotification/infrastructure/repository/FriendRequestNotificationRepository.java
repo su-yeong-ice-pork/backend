52e7fe44fc3557aedb938f5d353e628f258e4a7f
MinseokGo <rhalstjr1999@naver.com>
2024-11-21 18:41:01 +0900

    feat: 친구 요청 알림과 친구를 fetch join 해서 가져오는 JPA 메서드 작성

--- src/main/java/develop/grassserver/requestNotification/infrastructure/repository/FriendRequestNotificationRepository.java
+++ src/main/java/develop/grassserver/requestNotification/infrastructure/repository/FriendRequestNotificationRepository.java
@@ -2,6 +2,7 @@ package develop.grassserver.requestNotification.infrastructure.repository;
 
 import develop.grassserver.requestNotification.domain.entity.FriendRequestNotification;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +13,7 @@ public interface FriendRequestNotificationRepository extends JpaRepository<Frien
             + "from FriendRequestNotification frn join fetch frn.friend "
             + "where frn.friend.id in :friendRelationIds")
     List<FriendRequestNotification> findAllByFriends(@Param("friendRelationIds") List<Long> friendRelationIds);
+
+    @Query("select frn from FriendRequestNotification frn join fetch frn.friend where frn.id = :id")
+    Optional<FriendRequestNotification> findByIdWithFriend(@Param("id") Long id);
 }
