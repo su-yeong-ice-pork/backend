6671405b0bad456910d1c9cd01085e4f588f8263
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 15:48:22 +0900

    feat: studyRepository 생성

---
+++ src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
@@ -0,0 +1,7 @@
+package develop.grassserver.study.infrastructure.repository;
+
+import develop.grassserver.study.domain.entity.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyRepository extends JpaRepository<Study, Long> {
+}
