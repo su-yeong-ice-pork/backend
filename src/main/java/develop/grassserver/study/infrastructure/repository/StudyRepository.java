a57d2f159fb430bf268829a21ed11aa937ee408b
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 17:12:09 +0900

    feat: 스터디 초대코드 존재 확인 함수 구현

--- src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
+++ src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
@@ -4,4 +4,5 @@ import develop.grassserver.study.domain.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
+    boolean existsByInviteCode(String inviteCode);
 }
