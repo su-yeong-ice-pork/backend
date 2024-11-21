ac1a34b1ea3c993ef4c98213f4beb7fe4f31b156
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 11:13:02 +0900

    feat: randomStudyMemberRepository 추가

---
+++ src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyMemberRepository.java
@@ -0,0 +1,11 @@
+package develop.grassserver.randomStudy.infrastructure.repository;
+
+import develop.grassserver.randomStudy.domain.entity.RandomStudyMember;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RandomStudyMemberRepository extends JpaRepository<RandomStudyMember, Long> {
+    List<RandomStudyMember> findByRandomStudyId(Long randomStudyId);
+}
