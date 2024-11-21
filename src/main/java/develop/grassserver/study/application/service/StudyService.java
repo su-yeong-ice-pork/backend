939dc65f018a8ccb212826f550f5ea9966b8cdd7
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 18:11:46 +0900

    feat: 고정 스터디 상세 조회 기능 구현

--- src/main/java/develop/grassserver/study/application/service/StudyService.java
+++ src/main/java/develop/grassserver/study/application/service/StudyService.java
@@ -1,11 +1,15 @@
 package develop.grassserver.study.application.service;
 
 import develop.grassserver.member.domain.entity.Member;
+import develop.grassserver.study.application.service.exception.NotAStudyMemberException;
 import develop.grassserver.study.domain.entity.Study;
 import develop.grassserver.study.domain.entity.StudyRole;
+import develop.grassserver.study.infrastructure.repository.StudyMemberRepository;
 import develop.grassserver.study.infrastructure.repository.StudyRepository;
 import develop.grassserver.study.presentation.dto.CreateStudyRequest;
 import develop.grassserver.study.presentation.dto.CreateStudyResponse;
+import develop.grassserver.study.presentation.dto.StudyDetailResponse;
+import jakarta.persistence.EntityNotFoundException;
 import java.security.SecureRandom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +22,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class StudyService {
     private final StudyRepository studyRepository;
+    private final StudyMemberRepository studyMemberRepository;
+
+    public StudyDetailResponse getStudyDetail(Member member, Long studyId) {
+        if (!studyMemberRepository.existsByMemberIdAndStudyId(member.getId(), studyId)) {
+            throw new NotAStudyMemberException();
+        }
+
+        Study study = studyRepository.findById(studyId)
+                .orElseThrow(() -> new EntityNotFoundException("해당하는 스터디를 찾을 수 없습니다."));
+
+        return StudyDetailResponse.from(study);
+    }
 
     @Transactional
     public CreateStudyResponse createStudy(Member member, CreateStudyRequest request) {
