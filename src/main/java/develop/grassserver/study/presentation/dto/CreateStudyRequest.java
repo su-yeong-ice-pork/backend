976a067db67bcc52de85cb9fee5f9709ec37ae89
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 15:57:36 +0900

    feat: createStudyRequest에 엔티티 맵퍼 함수 생성

--- src/main/java/develop/grassserver/study/presentation/dto/CreateStudyRequest.java
+++ src/main/java/develop/grassserver/study/presentation/dto/CreateStudyRequest.java
@@ -1,8 +1,19 @@
 package develop.grassserver.study.presentation.dto;
 
+import develop.grassserver.study.domain.entity.Study;
+
 public record CreateStudyRequest(
         String name,
         String goalMessage,
         long goalTime
 ) {
+
+    public static Study from(CreateStudyRequest request) {
+        return Study.builder()
+                .name(request.name())
+                .goalMessage(request.goalMessage())
+                .goalTime(request.goalTime())
+                .build();
+
+    }
 }
