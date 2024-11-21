870d504b396f53c081a7192ca04fe67e3a0b4f0a
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-22 03:05:42 +0900

    feat: FindAllRandomStudyMembersResponse 엔티티 매핑 함수 작성

--- src/main/java/develop/grassserver/randomStudy/presentation/dto/FindAllRandomStudyMembersResponse.java
+++ src/main/java/develop/grassserver/randomStudy/presentation/dto/FindAllRandomStudyMembersResponse.java
@@ -1,10 +1,28 @@
 package develop.grassserver.randomStudy.presentation.dto;
 
+import develop.grassserver.grass.application.dto.MemberStudyInfoDTO;
 import java.util.List;
 
 public record FindAllRandomStudyMembersResponse(
         List<ParticipantDTO> participants
 ) {
+
+    public static FindAllRandomStudyMembersResponse from(MemberStudyInfoDTO studyInfo) {
+        return new FindAllRandomStudyMembersResponse(
+                studyInfo.members().stream()
+                        .map(member ->
+                                new ParticipantDTO(
+                                        member.getId(),
+                                        member.getName(),
+                                        member.getProfile().getImage(),
+                                        studyInfo.studyTimes().get(member.getId()),
+                                        studyInfo.studyStatuses().get(member.getId())
+                                )
+                        )
+                        .toList()
+        );
+    }
+
     public record ParticipantDTO(
             Long memberId,
             String name,
