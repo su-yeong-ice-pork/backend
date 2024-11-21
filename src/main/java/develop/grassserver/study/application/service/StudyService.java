8bc485f5a5224b5d6fe67fb6aea8119ea81ea08a
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-21 17:12:39 +0900

    feat: 랜덤 초대 코드 생성 로직 작성

--- src/main/java/develop/grassserver/study/application/service/StudyService.java
+++ src/main/java/develop/grassserver/study/application/service/StudyService.java
@@ -5,6 +5,7 @@ import develop.grassserver.study.domain.entity.Study;
 import develop.grassserver.study.domain.entity.StudyRole;
 import develop.grassserver.study.infrastructure.repository.StudyRepository;
 import develop.grassserver.study.presentation.dto.CreateStudyRequest;
+import java.security.SecureRandom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,9 +24,28 @@ public class StudyService {
                 .name(request.name())
                 .goalMessage(request.goalMessage())
                 .goalTime(request.goalTime())
+                .inviteCode(generateUniqueInviteCode())
                 .build();
 
         study.addMember(member, StudyRole.LEADER);
         studyRepository.save(study);
+    }
+
+    private String generateUniqueInviteCode() {
+        String code;
+        do {
+            code = generateRandomCode(6) + System.currentTimeMillis();
+        } while (studyRepository.existsByInviteCode(code));
+        return code;
+    }
+
+    private String generateRandomCode(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
     }
 }
