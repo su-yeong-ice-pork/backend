c224d75766d6207ae83f1f0982b9d6320e6928d5
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-29 03:07:21 +0900

    refactor: grassService에서 기간 조회 함수명 수정

--- src/main/java/develop/grassserver/grass/application/service/GrassService.java
+++ src/main/java/develop/grassserver/grass/application/service/GrassService.java
@@ -78,13 +78,13 @@ public class GrassService {
         return grassRepository.countByMemberId(memberId);
     }
 
-    public List<Grass> findYearlyGrass(Member member, int year) {
+    public List<Grass> findYearlyGrassByMemberId(Member member, int year) {
         LocalDate startDate = LocalDate.of(year, 1, 1);
         LocalDate endDate = LocalDate.of(year, 12, 31);
         return grassRepository.findByMemberAndAttendanceDateBetween(member, startDate, endDate);
     }
 
-    public List<Grass> findMonthlyGrass(Member member, int year, int month) {
+    public List<Grass> findMonthlyGrassByMemberId(Member member, int year, int month) {
         LocalDate startDate = LocalDate.of(year, month, 1);
         LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth());
         return grassRepository.findByMemberAndAttendanceDateBetween(member, startDate, endDate);
