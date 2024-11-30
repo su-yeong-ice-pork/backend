c6bf3228186fc49012fe7b71d435716224640835
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-30 23:18:34 +0900

    fix: RandomStudyRepository 레포지토리 어노테이션 추가

--- src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyRepository.java
+++ src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyRepository.java
@@ -2,6 +2,8 @@ package develop.grassserver.randomStudy.infrastructure.repository;
 
 import develop.grassserver.randomStudy.domain.entity.RandomStudy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RandomStudyRepository extends JpaRepository<RandomStudy, Long> {
 }
