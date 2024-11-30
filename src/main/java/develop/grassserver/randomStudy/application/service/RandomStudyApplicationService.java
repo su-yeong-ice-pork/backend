105d5820a8629830546865e865a67faeba42741a
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-01 03:29:14 +0900

    feat: 랜덤 스터디 신청 기능 구현

--- src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyApplicationService.java
+++ src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyApplicationService.java
@@ -1,8 +1,26 @@
 package develop.grassserver.randomStudy.application.service;
 
+import develop.grassserver.member.domain.entity.Member;
+import develop.grassserver.randomStudy.domain.entity.RandomStudyApplication;
+import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyApplicationRepository;
+import develop.grassserver.randomStudy.presentation.dto.ApplyRandomStudyRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RandomStudyApplicationService {
+    private final RandomStudyApplicationRepository applicationRepository;
 
+    public void applyRandomStudy(Member member, ApplyRandomStudyRequest request) {
+        RandomStudyApplication application = RandomStudyApplication.builder()
+                .member(member)
+                .attendanceDate(request.attendanceDate())
+                .attendanceTime(request.attendanceTime())
+                .build();
+
+        applicationRepository.save(application);
+    }
 }
