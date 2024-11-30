ad8800f99a4e69f3b3c6c6ce1fad8233c3eb8bae
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-01 03:37:02 +0900

    feat: 랜덤 스터디 신청 시 해당 날짜 중복 신청 확인 로직 추가

--- src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyApplicationService.java
+++ src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyApplicationService.java
@@ -2,9 +2,11 @@ package develop.grassserver.randomStudy.application.service;
 
 import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.randomStudy.application.exception.ApplicationDeadlinePassedException;
+import develop.grassserver.randomStudy.application.exception.DuplicateApplicationException;
 import develop.grassserver.randomStudy.domain.entity.RandomStudyApplication;
 import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyApplicationRepository;
 import develop.grassserver.randomStudy.presentation.dto.ApplyRandomStudyRequest;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ public class RandomStudyApplicationService {
 
     public void applyRandomStudy(Member member, ApplyRandomStudyRequest request) {
         validateApplicationDeadline();
+        checkDuplicateApplication(member, request.attendanceDate());
 
         RandomStudyApplication application = RandomStudyApplication.builder()
                 .member(member)
@@ -35,6 +38,13 @@ public class RandomStudyApplicationService {
 
         if (now.isAfter(deadline)) {
             throw new ApplicationDeadlinePassedException();
+        }
+    }
+
+    private void checkDuplicateApplication(Member member, LocalDate attendanceDate) {
+        boolean exist = applicationRepository.existsByMemberAndAttendanceDate(member, attendanceDate);
+        if (exist) {
+            throw new DuplicateApplicationException();
         }
     }
 }
--- src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyApplicationRepository.java
+++ src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyApplicationRepository.java
@@ -1,9 +1,12 @@
 package develop.grassserver.randomStudy.infrastructure.repository;
 
+import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.randomStudy.domain.entity.RandomStudyApplication;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RandomStudyApplicationRepository extends JpaRepository<RandomStudyApplication, Long> {
+    boolean existsByMemberAndAttendanceDate(Member member, LocalDate attendanceDate);
 }
