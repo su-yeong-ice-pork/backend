055a0c8296f7e77eb568d9656f30850ca063ae2e
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-01 03:33:49 +0900

    feat: 랜덤 스터디 신청 시 기준 시간이 지났는지 확인하는 로직 추가

--- src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyApplicationService.java
+++ src/main/java/develop/grassserver/randomStudy/application/service/RandomStudyApplicationService.java
@@ -1,9 +1,11 @@
 package develop.grassserver.randomStudy.application.service;
 
 import develop.grassserver.member.domain.entity.Member;
+import develop.grassserver.randomStudy.application.exception.ApplicationDeadlinePassedException;
 import develop.grassserver.randomStudy.domain.entity.RandomStudyApplication;
 import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyApplicationRepository;
 import develop.grassserver.randomStudy.presentation.dto.ApplyRandomStudyRequest;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class RandomStudyApplicationService {
+    public static final int DEADLINE_HOUR = 5;
     private final RandomStudyApplicationRepository applicationRepository;
 
     public void applyRandomStudy(Member member, ApplyRandomStudyRequest request) {
+        validateApplicationDeadline();
+
         RandomStudyApplication application = RandomStudyApplication.builder()
                 .member(member)
                 .attendanceDate(request.attendanceDate())
@@ -22,5 +27,14 @@ public class RandomStudyApplicationService {
                 .build();
 
         applicationRepository.save(application);
+    }
+
+    private void validateApplicationDeadline() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime deadline = now.withHour(DEADLINE_HOUR);
+
+        if (now.isAfter(deadline)) {
+            throw new ApplicationDeadlinePassedException();
+        }
     }
 }
