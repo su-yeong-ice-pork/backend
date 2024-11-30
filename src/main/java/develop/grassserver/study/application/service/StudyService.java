cf666728e2d959245fdc3dc26847992d24cb6d93
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-11-30 23:30:47 +0900

    chore: StudyService에서 불필요한 로깅 어노테이션 삭제

--- src/main/java/develop/grassserver/study/application/service/StudyService.java
+++ src/main/java/develop/grassserver/study/application/service/StudyService.java
@@ -18,11 +18,9 @@ import java.security.SecureRandom;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
