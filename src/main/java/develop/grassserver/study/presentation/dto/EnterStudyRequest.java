9cfc71678d963b71e6ee74b988a6c408e8850794
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-01 01:39:56 +0900

    feat: study 관련 dto 유효성 검증 어노테이션 추가

--- src/main/java/develop/grassserver/study/presentation/dto/EnterStudyRequest.java
+++ src/main/java/develop/grassserver/study/presentation/dto/EnterStudyRequest.java
@@ -1,4 +1,10 @@
 package develop.grassserver.study.presentation.dto;
 
-public record EnterStudyRequest(String inviteCode) {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record EnterStudyRequest(
+        @Schema(description = "스터디 초대코드", example = "GSDS234")
+        @NotBlank(message = "초대코드는 필수 항목입니다.")
+        String inviteCode) {
 }
