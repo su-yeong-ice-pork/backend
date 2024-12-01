702510776e9df53afbd332ad30cb5f73f9a01ab5
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-02 03:11:36 +0900

    feat: 랜덤 스터디 매칭 step 생성

--- src/main/java/develop/grassserver/common/config/BatchConfig.java
+++ src/main/java/develop/grassserver/common/config/BatchConfig.java
@@ -1,5 +1,6 @@
 package develop.grassserver.common.config;
 
+import develop.grassserver.randomStudy.domain.entity.RandomStudyApplication;
 import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyMemberRepository;
 import develop.grassserver.randomStudy.infrastructure.repository.RandomStudyRepository;
 import jakarta.persistence.EntityManagerFactory;
@@ -9,22 +10,37 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing
 @RequiredArgsConstructor
 public class BatchConfig {
 
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
     private final EntityManagerFactory entityManagerFactory;
     private final RandomStudyRepository randomStudyRepository;
     private final RandomStudyMemberRepository randomStudyMemberRepository;
 
     @Bean
-    public Job randomStudyMatchingJob(JobRepository jobRepository, Step matchingStep) {
+    public Job randomStudyMatchingJob(Step matchingStep) {
         return new JobBuilder("randomStudyMatchingJob", jobRepository)
                 .start(matchingStep)
                 .build();
+    }
+
+    @Bean
+    public Step matchingStep() {
+        return new StepBuilder("matchingStep", jobRepository)
+                .<RandomStudyApplication, RandomStudyApplication>chunk(100, transactionManager)
+                .reader(applicationItemReader())
+                .processor(applicationItemProcessor())
+                .writer(randomStudyItemWriter())
+                .build();
+
     }
 }
