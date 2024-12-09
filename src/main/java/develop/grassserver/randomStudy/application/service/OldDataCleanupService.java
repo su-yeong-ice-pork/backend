eed4ef845f7f3ca10e4ed74cbcf1e8ad89698832
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-10 02:46:23 +0900

    feat: 전날 랜덤 스터디 삭제

--- src/main/java/develop/grassserver/randomStudy/application/service/OldDataCleanupService.java
+++ src/main/java/develop/grassserver/randomStudy/application/service/OldDataCleanupService.java
@@ -13,11 +13,18 @@ public class OldDataCleanupService {
 
     private final EntityManager entityManager;
 
-    public int softDeleteOldRandomStudyApplications(LocalDate targetDate) {
+    public int deleteOldRandomStudyApplications(LocalDate targetDate) {
         return entityManager.createQuery(
                         "UPDATE RandomStudyApplication ra SET ra.status = false WHERE ra.attendanceDate = :targetDate AND ra.status = true"
                 )
                 .setParameter("targetDate", targetDate)
+                .executeUpdate();
+    }
+
+    public int deleteOldRandomStudies() {
+        return entityManager.createQuery(
+                        "UPDATE RandomStudy rs SET rs.status = false WHERE rs.status = true"
+                )
                 .executeUpdate();
     }
 }
