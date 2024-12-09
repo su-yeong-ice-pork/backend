9a009597b60369d9097cbfd2590172495ac49851
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-09 23:47:49 +0900

    feat: member로 당일 랜덤 스터디 조회 함수 구현

--- src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyRepository.java
+++ src/main/java/develop/grassserver/randomStudy/infrastructure/repository/RandomStudyRepository.java
@@ -1,9 +1,23 @@
 package develop.grassserver.randomStudy.infrastructure.repository;
 
+import develop.grassserver.member.domain.entity.Member;
 import develop.grassserver.randomStudy.domain.entity.RandomStudy;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RandomStudyRepository extends JpaRepository<RandomStudy, Long> {
+
+    @Query("SELECT rs FROM RandomStudy rs " +
+            "JOIN RandomStudyMember rsm ON rs = rsm.randomStudy " +
+            "WHERE rsm.status = TRUE AND rs.status = TRUE AND rsm.member = :member " +
+            "AND rs.attendanceTime BETWEEN :startOfDay AND :endOfDay")
+    Optional<RandomStudy> findByMemberAndToday(@Param("member") Member member,
+                                               @Param("startOfDay") LocalDateTime startOfDay,
+                                               @Param("endOfDay") LocalDateTime endOfDay);
+
 }
