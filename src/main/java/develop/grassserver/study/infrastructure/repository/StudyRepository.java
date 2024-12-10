1abe16c8f4cda2be53ae61c242d53d24574dbec2
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-10 16:24:11 +0900

    fix: fetch join시 스터디 인원수 오류 수정

--- src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
+++ src/main/java/develop/grassserver/study/infrastructure/repository/StudyRepository.java
@@ -10,9 +10,10 @@ import org.springframework.data.repository.query.Param;
 public interface StudyRepository extends JpaRepository<Study, Long> {
     boolean existsByInviteCode(String inviteCode);
 
-    @Query("SELECT s FROM Study s " +
-            "JOIN s.members sm " +
-            "WHERE sm.status = TRUE AND s.status = TRUE AND sm.member.id = :memberId")
+    @Query("SELECT DISTINCT s FROM Study s " +
+            "JOIN s.members smFilter " + // 필터링을 위한 조인
+            "JOIN FETCH s.members sm " +  // 페칭을 위한 조인
+            "WHERE s.status = TRUE AND smFilter.status = TRUE AND smFilter.member.id = :memberId")
     List<Study> findStudiesByMemberId(@Param("memberId") Long memberId);
 
     Optional<Study> findStudyByInviteCode(String inviteCode);
