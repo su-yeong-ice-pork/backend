9abc09213adcaa5c8c40e4423b3a8d356e2527b0
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-10 17:42:51 +0900

    refactor: log concat 제거

--- src/main/java/develop/grassserver/common/scheduler/RandomStudyDataCleanupScheduler.java
+++ src/main/java/develop/grassserver/common/scheduler/RandomStudyDataCleanupScheduler.java
@@ -1,14 +1,15 @@
 package develop.grassserver.common.scheduler;
 
+import develop.grassserver.common.annotation.Scheduler;
 import develop.grassserver.randomStudy.application.service.RandomStudyDataCleanupService;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 
-@Component
-@RequiredArgsConstructor
 @Slf4j
+@Scheduler
+@RequiredArgsConstructor
 public class RandomStudyDataCleanupScheduler {
 
     private final RandomStudyDataCleanupService randomStudyDataCleanupService;
@@ -27,10 +28,4 @@ public class RandomStudyDataCleanupScheduler {
             log.error("전일 데이터 정리 스케줄러 에러 발생: {}", e.getMessage(), e);
         }
     }
-
-    //    로컬에서 바로 테스트 해보고 싶을 때 실행
-//    @PostConstruct
-//    public void runOnStartup() {
-//        cleanUpOldData();
-//    }
 }
