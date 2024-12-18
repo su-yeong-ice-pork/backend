7070cb6c7d8e7166809d4383f76da0ef4b17b664
MinseokGo <rhalstjr1999@naver.com>
2024-12-18 16:30:02 +0900

    feat: 개인 랭킹 조회 서비스 로직 작성

--- src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingService.java
+++ src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingService.java
@@ -2,7 +2,6 @@ package develop.grassserver.rank.application.service;
 
 import develop.grassserver.auth.application.service.RedisService;
 import develop.grassserver.grass.application.dto.MemberStudyInfoDTO;
-import develop.grassserver.grass.application.service.GrassScoreAggregateService;
 import develop.grassserver.grass.application.service.MemberStudyInfoService;
 import develop.grassserver.grass.domain.entity.GrassScoreAggregate;
 import develop.grassserver.grass.infrastructure.repositiory.GrassScoreAggregateRepository;
@@ -19,28 +18,17 @@ public class GrassScoreRankingService {
 
     private final RedisService redisService;
     private final MemberStudyInfoService memberStudyInfoService;
-    private final GrassScoreAggregateService grassScoreAggregateService;
 
     private final GrassScoreAggregateRepository grassScoreAggregateRepository;
 
     public GrassScoreIndividualRankingResponse getIndividualRanking() {
         List<Long> rankingIds = redisService.getIndividualGrassScoreRanking();
-        List<GrassScoreAggregate> grassScoreAggregates = grassScoreAggregateRepository.findAllByIds(rankingIds);
 
-        // 1. <Rank, Score> Map -> DTO 파라미터 넘김
-        // 2. Ids, grassScoreAggregates 넘겨서 하드 코딩
+        List<GrassScoreAggregate> aggregates = grassScoreAggregateRepository.findAllByIds(rankingIds);
+        List<Long> memberIds = getMemberIds(aggregates);
+        MemberStudyInfoDTO memberStudyInfoDTO = memberStudyInfoService.getMemberStudyInfo(memberIds);
 
-        List<Long> memberIds = getMemberIds(grassScoreAggregates);
-
-        MemberStudyInfoDTO memberStudyInfo = memberStudyInfoService.getMemberStudyInfo(
-                getMemberIds(grassScoreAggregates)
-        );
-
-
-
-        // 1. 잔디 점수 집계를 가져옴(rankingIds)
-        // (2) GrassScoreAggregate + Member join Profile -> SELECT 2회, 로직 재사용
-        return null;
+        return GrassScoreIndividualRankingResponse.from(rankingIds, aggregates, memberStudyInfoDTO);
     }
 
     private List<Long> getMemberIds(List<GrassScoreAggregate> grassScoreAggregates) {
@@ -49,6 +37,4 @@ public class GrassScoreRankingService {
                 .boxed()
                 .toList();
     }
-
-
 }
