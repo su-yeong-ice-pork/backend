c120243b1e98e98c535cb1682c2c7a5e16c2a0ac
MinseokGo <rhalstjr1999@naver.com>
2024-12-18 21:13:27 +0900

    feat: 랭킹 날짜 포매터 작성

--- src/main/java/develop/grassserver/common/utils/DateTimeUtils.java
+++ src/main/java/develop/grassserver/common/utils/DateTimeUtils.java
@@ -1,12 +1,13 @@
 package develop.grassserver.common.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public final class DateTimeUtils {
 
     private static final DateTimeFormatter NOTIFICATION_TIME_FORMATTER = DateTimeFormatter.ofPattern("오늘 a hh:mm");
-    private static final DateTimeFormatter NOTIFICATION_DATE_FORMATTER = DateTimeFormatter.ofPattern("MM월 dd일");
+    private static final DateTimeFormatter MONTH_AND_DATE_FORMATTER = DateTimeFormatter.ofPattern("MM월 dd일");
 
     private DateTimeUtils() {
     }
@@ -21,6 +22,10 @@ public final class DateTimeUtils {
     }
 
     public static String formatNotificationDate(LocalDateTime createdAt) {
-        return createdAt.format(NOTIFICATION_DATE_FORMATTER);
+        return createdAt.format(MONTH_AND_DATE_FORMATTER);
+    }
+
+    public static String formatRankingDate(LocalDate yesterday) {
+        return yesterday.format(MONTH_AND_DATE_FORMATTER);
     }
 }
