8cf0588cb42499ee071a741209aa2f57e175ddc3
MinseokGo <rhalstjr1999@naver.com>
2024-12-18 20:43:39 +0900

    refactor: 안 쓰는 로깅 애노테이션 제거

--- src/main/java/develop/grassserver/common/scheduler/GrassScoreRankingScheduler.java
+++ src/main/java/develop/grassserver/common/scheduler/GrassScoreRankingScheduler.java
@@ -3,10 +3,8 @@ package develop.grassserver.common.scheduler;
 import develop.grassserver.common.annotation.Scheduler;
 import develop.grassserver.grass.application.service.GrassScoreAggregateService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 
-@Slf4j
 @Scheduler
 @RequiredArgsConstructor
 public class GrassScoreRankingScheduler {
