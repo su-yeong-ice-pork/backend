924f602fe05a55f9ec53125e0617d16c6260230b
MinseokGo <rhalstjr1999@naver.com>
2024-12-18 16:30:11 +0900

    feat: 개인 랭킹 조회 응답 DTO 작성

--- src/main/java/develop/grassserver/rank/presentation/dto/GrassScoreIndividualRankingResponse.java
+++ src/main/java/develop/grassserver/rank/presentation/dto/GrassScoreIndividualRankingResponse.java
@@ -2,10 +2,11 @@ package develop.grassserver.rank.presentation.dto;
 
 import develop.grassserver.grass.application.dto.MemberStudyInfoDTO;
 import develop.grassserver.grass.domain.entity.GrassScoreAggregate;
-import jakarta.persistence.EntityNotFoundException;
-import java.util.ArrayList;
+import develop.grassserver.member.domain.entity.Member;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public record GrassScoreIndividualRankingResponse(List<IndividualRank> ranking) {
 
@@ -14,34 +15,65 @@ public record GrassScoreIndividualRankingResponse(List<IndividualRank> ranking) 
             List<GrassScoreAggregate> grassScoreAggregates,
             MemberStudyInfoDTO memberStudyInfoDTO
     ) {
-        return new GrassScoreIndividualRankingResponse(
+        Map<Long, GrassScoreAggregate> aggregateMap = grassScoreAggregates.stream()
+                .collect(Collectors.toMap(
+                        GrassScoreAggregate::getId,
+                        grassScoreAggregate -> grassScoreAggregate
+                ));
 
-        )
+        Map<Long, Member> rankingMemberMap = grassScoreAggregates.stream()
+                .collect(Collectors.toMap(
+                        GrassScoreAggregate::getId,
+                        GrassScoreAggregate::getMember
+                ));
+
+        AtomicInteger rank = new AtomicInteger(1);
+        List<IndividualRank> ranks =
+                getIndividualRanks(rankingIds, memberStudyInfoDTO, rank, rankingMemberMap, aggregateMap);
+
+        return new GrassScoreIndividualRankingResponse(ranks);
     }
 
-    private List<IndividualRank> get(
+    private static List<IndividualRank> getIndividualRanks(
             List<Long> rankingIds,
-            List<GrassScoreAggregate> grassScoreAggregates,
-            MemberStudyInfoDTO memberStudyInfoDTO
+            MemberStudyInfoDTO memberStudyInfoDTO,
+            AtomicInteger rank,
+            Map<Long, Member> rankingMemberMap,
+            Map<Long, GrassScoreAggregate> aggregateMap
     ) {
-        List<IndividualRank> ranks = new ArrayList<>();
-        int i = 1;
-        for (Long rankingId : rankingIds) {
-            memberStudyInfoDTO.members().stream()
-                    .filter(member ->
-                            Objects.equals(member.getId(), grassScoreAggregates.stream()
-                                    .filter(grassScoreAggregate ->
-                                            Objects.equals(grassScoreAggregate.getId(), rankingId)
-                                    )
-                                    .findFirst()
-                                    .orElseThrow(() -> new EntityNotFoundException("버그")).getId())
-                    )
-                    .findFirst()
-                    .orElseThrow(() -> new EntityNotFoundException("버그"));
-            new IndividualRank(i, )
-        }
+        return rankingIds.stream()
+                .map(rankingId ->
+                        createIndividualRank(
+                                rank,
+                                memberStudyInfoDTO,
+                                rankingId,
+                                rankingMemberMap,
+                                aggregateMap
+                        )
+                )
+                .toList();
     }
 
+    private static IndividualRank createIndividualRank(
+            AtomicInteger rank,
+            MemberStudyInfoDTO memberStudyInfoDTO,
+            Long rankingId,
+            Map<Long, Member> rankingMemberMap,
+            Map<Long, GrassScoreAggregate> aggregateMap
+    ) {
+        Member findMember = rankingMemberMap.get(rankingId);
+        Long findMemberId = findMember.getId();
+
+        return new IndividualRank(
+                rank.getAndIncrement(),
+                findMemberId,
+                findMember.getName(),
+                findMember.getProfile().getImage(),
+                memberStudyInfoDTO.studyTimes().get(findMemberId),
+                memberStudyInfoDTO.studyStatuses().get(findMemberId),
+                aggregateMap.get(rankingId).getGrassScore()
+        );
+    }
 
     public record IndividualRank(
             int rank,
