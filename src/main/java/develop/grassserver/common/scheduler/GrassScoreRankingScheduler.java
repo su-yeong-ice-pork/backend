1ae02a9a3e1c0d615e09ab0f4bc915134f1cb808
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-12-19 01:43:02 +0900

    feat: 스케줄러에 스터디 랭킹 저장 기능 추가

--- src/main/java/develop/grassserver/common/scheduler/GrassScoreRankingScheduler.java
+++ src/main/java/develop/grassserver/common/scheduler/GrassScoreRankingScheduler.java
@@ -2,6 +2,7 @@ package develop.grassserver.common.scheduler;
 
 import develop.grassserver.common.annotation.Scheduler;
 import develop.grassserver.grass.application.service.GrassScoreAggregateService;
+import develop.grassserver.study.application.service.StudyRankingCacheService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,9 +13,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class GrassScoreRankingScheduler {
 
     private final GrassScoreAggregateService grassScoreAggregateService;
+    private final StudyRankingCacheService studyRankingCacheService;
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void calculateRanking() {
         grassScoreAggregateService.calculateGrassScoreRanking();
+        studyRankingCacheService.calculateStudyRanking();
     }
 }
