6040c029bf307e6295902137376544d0e6ccfb7e
MinseokGo <rhalstjr1999@naver.com>
2025-01-10 16:17:45 +0900

    feat: 재시도 설정 애노테이션 추가

--- src/main/java/develop/grassserver/GrassServerApplication.java
+++ src/main/java/develop/grassserver/GrassServerApplication.java
@@ -3,8 +3,10 @@ package develop.grassserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRetry
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
