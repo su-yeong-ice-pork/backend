35b45a291a0c393505583f73531194f9161bc93b
MinseokGo <rhalstjr1999@naver.com>
2025-01-19 15:30:25 +0900

    refactor: 개인 랭킹 조회 API 수정

--- src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingService.java
+++ src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingService.java
@@ -1,40 +1,27 @@
 package develop.grassserver.rank.application.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import develop.grassserver.auth.application.service.RedisService;
-import develop.grassserver.grass.application.dto.MemberStudyInfoDTO;
-import develop.grassserver.grass.application.service.MemberStudyInfoService;
-import develop.grassserver.grass.domain.entity.GrassScoreAggregate;
-import develop.grassserver.grass.infrastructure.repositiory.GrassScoreAggregateRepository;
-import develop.grassserver.rank.presentation.dto.GrassScoreIndividualRankingResponse;
-import java.util.List;
+import develop.grassserver.rank.presentation.dto.IndividualRankingResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class GrassScoreRankingService {
 
     private final RedisService redisService;
-    private final MemberStudyInfoService memberStudyInfoService;
 
-    private final GrassScoreAggregateRepository grassScoreAggregateRepository;
-
-    public GrassScoreIndividualRankingResponse getIndividualRanking() {
-        List<Long> rankingIds = redisService.getIndividualGrassScoreRanking();
-
-        List<GrassScoreAggregate> aggregates = grassScoreAggregateRepository.findAllByIds(rankingIds);
-        List<Long> memberIds = getMemberIds(aggregates);
-        MemberStudyInfoDTO memberStudyInfoDTO = memberStudyInfoService.getMemberStudyInfo(memberIds);
-
-        return GrassScoreIndividualRankingResponse.from(rankingIds, aggregates, memberStudyInfoDTO);
-    }
-
-    private List<Long> getMemberIds(List<GrassScoreAggregate> grassScoreAggregates) {
-        return grassScoreAggregates.stream()
-                .mapToLong(grassScoreAggregate -> grassScoreAggregate.getMember().getId())
-                .boxed()
-                .toList();
+    public IndividualRankingResponse getIndividualRanking() {
+        try {
+            return redisService.getIndividualGrassScoreRanking();
+        } catch (JsonProcessingException exception) {
+            log.error("개인 랭킹 조회 중 예외 발생 = {}", exception.getMessage());
+            throw new IllegalStateException("서버 오류 발생");
+        }
     }
 }
