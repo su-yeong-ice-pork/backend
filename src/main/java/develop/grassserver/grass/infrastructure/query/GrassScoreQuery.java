a09435709620900a8fb13c31a094a4247eeac405
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-02-03 20:19:23 +0900

    feat: query class를 enum에서 일반 클래스로 변경

--- src/main/java/develop/grassserver/grass/infrastructure/query/GrassScoreQuery.java
+++ src/main/java/develop/grassserver/grass/infrastructure/query/GrassScoreQuery.java
@@ -3,22 +3,21 @@ package develop.grassserver.grass.infrastructure.query;
 import lombok.Getter;
 
 @Getter
-public enum GrassScoreQuery {
-    MEMBER_TOTAL_GRASS_SCORE_UPDATE(
+public final class GrassScoreQuery {
+
+    public static final String MEMBER_TOTAL_GRASS_SCORE_UPDATE_QUERY =
             "UPDATE grass_score_aggregate gsa " +
                     "JOIN grass g ON gsa.member_id = g.member_id " +
                     "SET gsa.grass_score = gsa.grass_score + g.grass_score, " +
                     "    gsa.updated_at = NOW() " +
                     "WHERE g.attendance_date = :yesterday " +
-                    "  AND g.status = true"
-    ),
-    MEMBER_TOTAL_GRASS_SCORE_SELECT(
-            "SELECT gsa FROM GrassScoreAggregate gsa WHERE gsa.member.status = TRUE ORDER BY gsa.grassScore DESC"
-    );
+                    "  AND g.status = true";
+    public static final String MEMBER_TOTAL_GRASS_SCORE_SELECT_QUERY =
+            "SELECT gsa FROM GrassScoreAggregate gsa " +
+                    "WHERE gsa.member.status = TRUE " +
+                    "ORDER BY gsa.grassScore DESC";
 
-    private final String query;
-
-    GrassScoreQuery(String query) {
-        this.query = query;
+    private GrassScoreQuery() {
     }
 }
+
