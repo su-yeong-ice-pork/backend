61afdd2b143e6ede206508f6a5a35a4a0f564fa4
MinseokGo <rhalstjr1999@naver.com>
2025-02-05 13:51:03 +0900

    refactor: 미사용 Getter 제거

--- src/main/java/develop/grassserver/grass/infrastructure/query/GrassScoreQuery.java
+++ src/main/java/develop/grassserver/grass/infrastructure/query/GrassScoreQuery.java
@@ -1,17 +1,15 @@
 package develop.grassserver.grass.infrastructure.query;
 
-import lombok.Getter;
-
-@Getter
 public final class GrassScoreQuery {
 
     public static final String MEMBER_TOTAL_GRASS_SCORE_UPDATE_QUERY =
             "UPDATE grass_score_aggregate gsa " +
                     "JOIN grass g ON gsa.member_id = g.member_id " +
                     "SET gsa.grass_score = gsa.grass_score + g.grass_score, " +
-                    "    gsa.updated_at = NOW() " +
+                    "gsa.updated_at = NOW() " +
                     "WHERE g.attendance_date = :yesterday " +
-                    "  AND g.status = true";
+                    "AND g.status = true";
+
     public static final String MEMBER_TOTAL_GRASS_SCORE_SELECT_QUERY =
             "SELECT gsa FROM GrassScoreAggregate gsa " +
                     "WHERE gsa.member.status = TRUE " +
