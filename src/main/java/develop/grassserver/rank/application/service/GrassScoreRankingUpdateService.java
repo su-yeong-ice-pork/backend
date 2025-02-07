250b03098b156f410ab8b9fab9e0aa493a021e52
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-02-07 23:23:43 +0900

    feat: 레디스 재시도를 어노테이션 적용

--- src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingUpdateService.java
+++ src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingUpdateService.java
@@ -2,25 +2,14 @@ package develop.grassserver.rank.application.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import develop.grassserver.auth.application.service.RedisService;
+import develop.grassserver.common.annotation.RedisRetryable;
 import develop.grassserver.grass.infrastructure.repositiory.GrassScoreAggregateQueryRepository;
 import develop.grassserver.rank.presentation.dto.IndividualRankingResponse;
 import develop.grassserver.rank.presentation.dto.StudyRankingResponse;
-import io.lettuce.core.RedisCommandTimeoutException;
-import io.lettuce.core.RedisConnectionException;
 import io.lettuce.core.RedisException;
-import jakarta.persistence.PersistenceException;
-import jakarta.persistence.QueryTimeoutException;
-import java.net.ConnectException;
-import java.net.SocketTimeoutException;
-import java.sql.SQLException;
-import java.sql.SQLTimeoutException;
 import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.JDBCConnectionException;
-import org.springframework.data.redis.RedisConnectionFailureException;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,35 +23,14 @@ public class GrassScoreRankingUpdateService {
 
     private final GrassScoreAggregateQueryRepository grassScoreAggregateQueryRepository;
 
-    @Retryable(
-            retryFor = {
-                    PersistenceException.class,
-                    SQLException.class,
-                    SQLTimeoutException.class,
-                    QueryTimeoutException.class,
-                    JDBCConnectionException.class
-            },
-            maxAttempts = 5,
-            backoff = @Backoff(delay = 5000)
-    )
+    @RedisRetryable
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void updateGrassAggregateScore() {
         LocalDate yesterday = LocalDate.now().minusDays(1);
         grassScoreAggregateQueryRepository.update(yesterday);
     }
 
-    @Retryable(
-            retryFor = {
-                    RedisConnectionFailureException.class,
-                    RedisConnectionException.class,
-                    RedisCommandTimeoutException.class,
-                    RedisException.class,
-                    SocketTimeoutException.class,
-                    ConnectException.class
-            },
-            maxAttempts = 5,
-            backoff = @Backoff(delay = 5000)
-    )
+    @RedisRetryable
     public void saveIndividualGrassScoreRanking(IndividualRankingResponse response) {
         try {
             redisService.saveIndividualGrassScoreRanking(response);
@@ -72,18 +40,7 @@ public class GrassScoreRankingUpdateService {
         }
     }
 
-    @Retryable(
-            retryFor = {
-                    RedisConnectionFailureException.class,
-                    RedisConnectionException.class,
-                    RedisCommandTimeoutException.class,
-                    RedisException.class,
-                    SocketTimeoutException.class,
-                    ConnectException.class
-            },
-            maxAttempts = 5,
-            backoff = @Backoff(delay = 5000)
-    )
+    @RedisRetryable
     public void saveStudyRanking(StudyRankingResponse response) {
         try {
             redisService.saveStudyRanking(response);
