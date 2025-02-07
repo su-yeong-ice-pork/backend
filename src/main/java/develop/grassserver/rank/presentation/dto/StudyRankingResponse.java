52cfe39c4f51ad87ed38b18871b41b5c2ef99a18
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-02-07 22:05:44 +0900

    feat: StudyRankingResponse from 함수 작성

--- src/main/java/develop/grassserver/rank/presentation/dto/StudyRankingResponse.java
+++ src/main/java/develop/grassserver/rank/presentation/dto/StudyRankingResponse.java
@@ -1,8 +1,25 @@
 package develop.grassserver.rank.presentation.dto;
 
+import develop.grassserver.common.utils.duration.DurationUtils;
+import develop.grassserver.study.domain.entity.Study;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public record StudyRankingResponse(List<StudyRank> ranking) {
+
+    public static StudyRankingResponse from(List<Study> studies) {
+        List<StudyRank> ranks = IntStream.range(0, studies.size())
+                .mapToObj(i -> new StudyRank(
+                        i + 1,
+                        studies.get(i).getName(),
+                        studies.get(i).getMembers().size(),
+                        DurationUtils.formatHourDuration(studies.get(i).getTotalStudyTime())
+                ))
+                .collect(Collectors.toList());
+
+        return new StudyRankingResponse(ranks);
+    }
 
     public record StudyRank(
             int rank,
