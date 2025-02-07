3c174d3fe786c68281daf28a52ce3f0bdd17386f
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-02-07 22:00:04 +0900

    feat: 스터디 랭킹 저장 함수 구현

--- src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingUpdateService.java
+++ src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingUpdateService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import develop.grassserver.auth.application.service.RedisService;
 import develop.grassserver.grass.infrastructure.repositiory.GrassScoreAggregateQueryRepository;
 import develop.grassserver.rank.presentation.dto.IndividualRankingResponse;
+import develop.grassserver.rank.presentation.dto.StudyRankingResponse;
 import io.lettuce.core.RedisCommandTimeoutException;
 import io.lettuce.core.RedisConnectionException;
 import io.lettuce.core.RedisException;
@@ -68,6 +69,27 @@ public class GrassScoreRankingUpdateService {
         } catch (JsonProcessingException e) {
             log.error("랭킹 JSON 저장 중 오류");
             throw new RedisException("랭킹 JSON 저장 중 오류");
+        }
+    }
+
+    @Retryable(
+            retryFor = {
+                    RedisConnectionFailureException.class,
+                    RedisConnectionException.class,
+                    RedisCommandTimeoutException.class,
+                    RedisException.class,
+                    SocketTimeoutException.class,
+                    ConnectException.class
+            },
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 5000)
+    )
+    public void saveStudyRanking(StudyRankingResponse response) {
+        try {
+            redisService.saveStudyRanking(response);
+        } catch (JsonProcessingException e) {
+            log.error("스터디 랭킹 JSON 저장 중 오류");
+            throw new RedisException("스터디 랭킹 JSON 저장 중 오류");
         }
     }
 }
