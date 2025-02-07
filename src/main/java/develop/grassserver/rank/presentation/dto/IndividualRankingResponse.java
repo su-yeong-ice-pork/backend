cc6c019aa52e8cb3bc8b6bbe8dd70f4e2175bbc6
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-02-07 23:18:24 +0900

    refactor: 개인 랭킹 response 변환함수 stream으로 변경

--- src/main/java/develop/grassserver/rank/presentation/dto/IndividualRankingResponse.java
+++ src/main/java/develop/grassserver/rank/presentation/dto/IndividualRankingResponse.java
@@ -5,18 +5,21 @@ import develop.grassserver.common.utils.duration.DurationUtils;
 import develop.grassserver.grass.domain.entity.GrassScoreAggregate;
 import develop.grassserver.member.domain.entity.Member;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public record IndividualRankingResponse(String date, List<IndividualRank> ranking) {
 
     public static IndividualRankingResponse from(List<GrassScoreAggregate> aggregates) {
-        List<IndividualRank> individualRanks = new ArrayList<>();
-        for (int i = 1; i <= aggregates.size(); i++) {
-            GrassScoreAggregate aggregate = aggregates.get(i - 1);
-            Member member = aggregate.getMember();
-            individualRanks.add(getIndividualRank(i, member, aggregate));
-        }
+        List<IndividualRank> individualRanks = IntStream.range(0, aggregates.size())
+                .mapToObj(i -> {
+                    GrassScoreAggregate aggregate = aggregates.get(i);
+                    Member member = aggregate.getMember();
+                    return getIndividualRank(i + 1, member, aggregate); // 순위는 1부터 시작하므로 i+1
+                })
+                .collect(Collectors.toList());
+
         String date = DateTimeUtils.formatNotificationDate(LocalDateTime.now().minusDays(1));
         return new IndividualRankingResponse(date, individualRanks);
     }
