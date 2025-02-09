b9174c16887a97b05fceac6eb326dfdad4058a5b
MinseokGo <rhalstjr1999@naver.com>
2025-02-09 17:20:09 +0900

    feat: 학과 랭킹 조회 컨트롤러 메서드 등록

--- src/main/java/develop/grassserver/rank/presentation/controller/GrassScoreRankingController.java
+++ src/main/java/develop/grassserver/rank/presentation/controller/GrassScoreRankingController.java
@@ -4,6 +4,7 @@ import develop.grassserver.common.utils.ApiUtils;
 import develop.grassserver.common.utils.ApiUtils.ApiResult;
 import develop.grassserver.rank.application.service.GrassScoreRankingService;
 import develop.grassserver.rank.presentation.dto.IndividualRankingResponse;
+import develop.grassserver.rank.presentation.dto.MajorRankingResponse;
 import develop.grassserver.rank.presentation.dto.StudyRankingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,13 @@ public class GrassScoreRankingController {
     @GetMapping("/studies")
     public ResponseEntity<ApiResult<StudyRankingResponse>> getStudyRanking() {
         StudyRankingResponse response = rankingService.getStudyRanking();
+        return ResponseEntity.ok()
+                .body(ApiUtils.success(response));
+    }
+
+    @GetMapping("/major")
+    public ResponseEntity<ApiResult<MajorRankingResponse>> getMajorRanking() {
+        MajorRankingResponse response = rankingService.getMajorRanking();
         return ResponseEntity.ok()
                 .body(ApiUtils.success(response));
     }
