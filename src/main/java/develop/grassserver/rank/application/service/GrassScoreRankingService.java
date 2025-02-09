3b19ddce186919d46a92190daebf73fc2dafd5a6
MinseokGo <rhalstjr1999@naver.com>
2025-02-09 17:19:57 +0900

    feat: 학과 랭킹 조회 서비스 로직 등록

--- src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingService.java
+++ src/main/java/develop/grassserver/rank/application/service/GrassScoreRankingService.java
@@ -3,6 +3,7 @@ package develop.grassserver.rank.application.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import develop.grassserver.auth.application.service.RedisService;
 import develop.grassserver.rank.presentation.dto.IndividualRankingResponse;
+import develop.grassserver.rank.presentation.dto.MajorRankingResponse;
 import develop.grassserver.rank.presentation.dto.StudyRankingResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +32,15 @@ public class GrassScoreRankingService {
             return redisService.getStudyRanking();
         } catch (JsonProcessingException exception) {
             log.error("스터디 랭킹 조회 중 예외 발생 = {}", exception.getMessage());
+            throw new IllegalStateException("서버 오류 발생");
+        }
+    }
+
+    public MajorRankingResponse getMajorRanking() {
+        try {
+            return redisService.getMajorRanking();
+        } catch (JsonProcessingException exception) {
+            log.error("학과 랭킹 조회 중 예외 발생 = {}", exception.getMessage());
             throw new IllegalStateException("서버 오류 발생");
         }
     }
