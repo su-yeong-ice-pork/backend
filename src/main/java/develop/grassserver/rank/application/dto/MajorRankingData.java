b3554256a4f68cf2fde07fcec49bb55f397aef5a
MinseokGo <rhalstjr1999@naver.com>
2025-02-09 17:06:36 +0900

    fix: DB 타입에 맞춰 타입 변환

--- src/main/java/develop/grassserver/rank/application/dto/MajorRankingData.java
+++ src/main/java/develop/grassserver/rank/application/dto/MajorRankingData.java
@@ -1,11 +1,9 @@
 package develop.grassserver.rank.application.dto;
 
-import java.time.Duration;
-
 public record MajorRankingData(
         String majorName,
-        int memberCount,
-        Duration majorTotalStudyTime,
-        int majorGrassScore
+        long memberCount,
+        long majorTotalStudyTime,
+        long majorGrassScore
 ) {
 }
