dea1425cc3a1a2d1051ab368ce619accb3554889
MinseokGo <rhalstjr1999@naver.com>
2025-03-14 17:30:16 +0900

    remove: 미사용 리포지토리 삭제

--- src/main/java/develop/grassserver/grass/infrastructure/repositiory/GrassScoreAggregateCustomRepository.java
+++
@@ -1,8 +0,0 @@
-package develop.grassserver.grass.infrastructure.repositiory;
-
-import develop.grassserver.member.domain.entity.Member;
-
-public interface GrassScoreAggregateCustomRepository {
-
-    long subtractScore(int score, Member member);
-}
