498ed833f5b76ba7c843679576bca500120d9132
MinseokGo <rhalstjr1999@naver.com>
2025-03-14 16:43:24 +0900

    feat: grassScoreAggregate 리포지토리 인터페이스 등록

--- src/main/java/develop/grassserver/grass/infrastructure/repositiory/GrassScoreAggregateRepository.java
+++ src/main/java/develop/grassserver/grass/infrastructure/repositiory/GrassScoreAggregateRepository.java
@@ -5,7 +5,8 @@ import develop.grassserver.member.domain.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GrassScoreAggregateRepository extends JpaRepository<GrassScoreAggregate, Long> {
+public interface GrassScoreAggregateRepository
+        extends JpaRepository<GrassScoreAggregate, Long>, GrassScoreAggregateCustomRepository {
 
     boolean existsByMember(Member member);
 
