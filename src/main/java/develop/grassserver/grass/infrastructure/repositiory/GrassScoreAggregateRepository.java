da99de34a0c5d57a615d33ddf57bce138f13368d
MinseokGo <rhalstjr1999@naver.com>
2025-03-14 17:30:42 +0900

    fix: 구현체 변경

--- src/main/java/develop/grassserver/grass/infrastructure/repositiory/GrassScoreAggregateRepository.java
+++ src/main/java/develop/grassserver/grass/infrastructure/repositiory/GrassScoreAggregateRepository.java
@@ -5,8 +5,7 @@ import develop.grassserver.member.domain.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GrassScoreAggregateRepository
-        extends JpaRepository<GrassScoreAggregate, Long>, GrassScoreAggregateCustomRepository {
+public interface GrassScoreAggregateRepository extends JpaRepository<GrassScoreAggregate, Long> {
 
     boolean existsByMember(Member member);
 
