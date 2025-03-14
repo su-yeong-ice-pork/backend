e5085268058655b0c9da36bc5936b4903b216a1c
Yoo KyeongMi <yookyungmi02@gmail.com>
2025-03-14 17:33:01 +0900

    feat: 프리즈 감소 exception 핸들러 처리

--- src/main/java/develop/grassserver/profile/presentation/exception/ProfileExceptionHandler.java
+++ src/main/java/develop/grassserver/profile/presentation/exception/ProfileExceptionHandler.java
@@ -2,6 +2,7 @@ package develop.grassserver.profile.presentation.exception;
 
 import develop.grassserver.common.utils.ApiUtils.ApiResult;
 import develop.grassserver.profile.application.exception.ImageUploadFailedException;
+import develop.grassserver.profile.application.exception.NotEnoughFreezeCountException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,11 @@ public class ProfileExceptionHandler {
 
     @ExceptionHandler(ImageUploadFailedException.class)
     public ResponseEntity<ApiResult<?>> imageUploadFailedException(ImageUploadFailedException exception) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+
+    @ExceptionHandler(NotEnoughFreezeCountException.class)
+    public ResponseEntity<ApiResult<?>> notEnoughFreezeCountException(ImageUploadFailedException exception) {
         return new ResponseEntity<>(exception.body(), exception.status());
     }
 }
